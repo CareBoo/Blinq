982e113e604e3eb33e27147a739011dc7017d82e
Jason Bou Kheir <jason.boukheir@pm.me>
2020-08-26 22:29:22 -0700

    feat: :sparkles: adding count
    Adding NativeSequence.Count

---
+++ Packages/com.careboo.blinq/Runtime/NativeSequence/NativeSequence.Count.cs
@@ -0,0 +1,45 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace CareBoo.Blinq
+{
+    public partial struct NativeSequence<T>
+        where T : struct
+    {
+        public int Count(BurstCompiledFunc<T, bool> predicate)
+        {
+            var output = new NativeArray<int>(1, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+            var job = new CountJob { Input = input, Predicate = predicate, Output = output };
+            job.Schedule(input.Length, 64, dependsOn).Complete();
+            var result = output[0];
+            output.Dispose();
+            return result;
+        }
+
+        public int Count()
+        {
+            return Length;
+        }
+
+        [BurstCompile(CompileSynchronously = true)]
+        public struct CountJob : IJobParallelFor
+        {
+            [ReadOnly]
+            [DeallocateOnJobCompletion]
+            public NativeArray<T> Input;
+
+            [ReadOnly]
+            public BurstCompiledFunc<T, bool> Predicate;
+
+            [NativeDisableParallelForRestriction]
+            public NativeArray<int> Output;
+
+            public void Execute(int index)
+            {
+                if (Predicate.Invoke(Input[index]))
+                    Output[0]++;
+            }
+        }
+    }
+}
