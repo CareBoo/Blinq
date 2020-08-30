49690addcb585bf498426aeb3a0cfb39ba78e845
Jason Bou Kheir <jason.boukheir@pm.me>
2020-08-30 14:39:19 -0700

    fix: :bug: enforce IFunc struct params

--- Packages/com.careboo.blinq/Runtime/IFunc.cs
+++ Packages/com.careboo.blinq/Runtime/IFunc.cs
@@ -1,30 +1,40 @@
 ﻿namespace CareBoo.Blinq
 {
-    public interface IFunc<TResult>
+    public interface IFunc<out TResult>
         where TResult : struct
     {
         TResult Invoke();
     }
 
-    public interface IFunc<T, TResult>
+    public interface IFunc<T, out TResult>
+        where T : struct
         where TResult : struct
     {
         TResult Invoke(T arg0);
     }
 
-    public interface IFunc<T, U, TResult>
+    public interface IFunc<T, U, out TResult>
+        where T : struct
+        where U : struct
         where TResult : struct
     {
         TResult Invoke(T arg0, U arg1);
     }
 
-    public interface IFunc<T, U, V, TResult>
+    public interface IFunc<T, U, V, out TResult>
+        where T : struct
+        where U : struct
+        where V : struct
         where TResult : struct
     {
         TResult Invoke(T arg0, U arg1, V arg2);
     }
 
-    public interface IFunc<T, U, V, W, TResult>
+    public interface IFunc<T, U, V, W, out TResult>
+        where T : struct
+        where U : struct
+        where V : struct
+        where W : struct
         where TResult : struct
     {
         TResult Invoke(T arg0, U arg1, V arg2, W arg3);
