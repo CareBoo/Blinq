81cb6d3b31df9e4da501989b02a1a62fca1156f9
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-03 23:24:08 -0700

    LF line endings

--- Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Distinct.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Distinct.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LambdaDiscoveryTests : MonoBehaviour
+public class ValueSequence.Distinct : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
