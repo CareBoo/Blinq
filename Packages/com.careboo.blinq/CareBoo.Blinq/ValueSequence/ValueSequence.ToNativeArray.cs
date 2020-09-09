22f7f15419accf7a65d3c56069dd760445fc2732
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-08 23:50:53 -0700

    feat: :sparkles: Add ToNativeArray and ToNativeArrayJob
    Fixes #31

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.ToNativeArray.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public partial struct ValueSequence<T, TQuery>
+        : IEnumerable<T>
+        where T : struct
+        where TQuery : struct, IQuery<T>
+    {
+        [BurstCompile]
+        public NativeArray<T> ToNativeArray(Allocator allocator)
+        {
+            var list = Execute();
+            var output = new NativeArray<T>(list, allocator);
+            return output;
+        }
+    }
+}
