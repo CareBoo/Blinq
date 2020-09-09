b17625750d231471933ef9d24d7a6728fd2b4eab
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-08 23:23:37 -0700

    feat: :sparkles: Add ToNativeHashMap and ToNativeHashMapJob

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.ToNativeHashMap.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public partial struct ValueSequence<T, TQuery>
+        : IEnumerable<T>
+        where T : struct
+        where TQuery : struct, IQuery<T>
+    {
+        [BurstCompile]
+        public NativeHashMap<TKey, TElement> ToNativeHashMap<TKey, TElement, TKeySelector, TElementSelector>(
+            TKeySelector keySelector,
+            TElementSelector elementSelector,
+            Allocator allocator
+            )
+            where TKey : struct, IEquatable<TKey>
+            where TElement : struct
+            where TKeySelector : struct, IValueFunc<T, TKey>
+            where TElementSelector : struct, IValueFunc<T, TElement>
+        {
+            var output = new NativeHashMap<TKey, TElement>(0, allocator);
+            ToNativeHashMap(keySelector, elementSelector, output);
+            return output;
+        }
+
+        [BurstCompile]
+        public void ToNativeHashMap<TKey, TElement, TKeySelector, TElementSelector>(
+            TKeySelector keySelector,
+            TElementSelector elementSelector,
+            NativeHashMap<TKey, TElement> output
+            )
+            where TKey : struct, IEquatable<TKey>
+            where TElement : struct
+            where TKeySelector : struct, IValueFunc<T, TKey>
+            where TElementSelector : struct, IValueFunc<T, TElement>
+        {
+            var list = Execute();
+            output.Capacity = list.Length;
+            for (var i = 0; i < list.Length; i++)
+            {
+                var key = keySelector.Invoke(list[i]);
+                var element = elementSelector.Invoke(list[i]);
+                if (!output.TryAdd(key, element))
+                    throw Error.DuplicateKey();
+            }
+        }
+
+        [BurstCompile]
+        public NativeHashMap<TKey, T> ToNativeHashMap<TKey, TKeySelector>(
+            TKeySelector keySelector,
+            Allocator allocator
+            )
+            where TKey : struct, IEquatable<TKey>
+            where TKeySelector : struct, IValueFunc<T, TKey>
+        {
+            var output = new NativeHashMap<TKey, T>(0, allocator);
+            ToNativeHashMap(keySelector, output);
+            return output;
+        }
+
+        [BurstCompile]
+        public void ToNativeHashMap<TKey, TKeySelector>(
+            TKeySelector keySelector,
+            NativeHashMap<TKey, T> output
+            )
+            where TKey : struct, IEquatable<TKey>
+            where TKeySelector : struct, IValueFunc<T, TKey>
+        {
+            var list = Execute();
+            output.Capacity = list.Length;
+            for (var i = 0; i < list.Length; i++)
+            {
+                var key = keySelector.Invoke(list[i]);
+                var element = list[i];
+                if (!output.TryAdd(key, element))
+                    throw Error.DuplicateKey();
+            }
+        }
+    }
+}
