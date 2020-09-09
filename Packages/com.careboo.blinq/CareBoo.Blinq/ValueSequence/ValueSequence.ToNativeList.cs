64e7b3250104c606fc9120828b3bb3f9d405d89b
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-08 23:23:21 -0700

    feat: :sparkles: Add ToNativeList and ToNativeListJob

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.ToNativeList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public partial struct ValueSequence<T, TQuery>
+        : IEnumerable<T>
+        where T : struct
+        where TQuery : struct, IQuery<T>
+    {
+        [BurstCompile]
+        public NativeList<T> ToNativeList(Allocator allocator)
+        {
+            var output = new NativeList<T>(allocator);
+            ToNativeList(output);
+            return output;
+        }
+
+        [BurstCompile]
+        public void ToNativeList(NativeList<T> output)
+        {
+            var list = Execute();
+            output.CopyFrom(list);
+            list.Dispose();
+        }
+    }
+}
---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.ToNativeListJob.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace CareBoo.Blinq
+{
+    public partial struct ValueSequence<T, TQuery>
+        : IEnumerable<T>
+        where T : struct
+        where TQuery : struct, IQuery<T>
+    {
+        public NativeListJob ToNativeListJob(NativeList<T> output)
+        {
+            return new NativeListJob { Sequence = this, Output = output };
+        }
+
+        public SequenceExecutionJobWrapper<T, NativeListJob, NativeList<T>> ToNativeListJob(Allocator allocator)
+        {
+            var output = new NativeList<T>(allocator);
+            var job = new NativeListJob { Sequence = this, Output = output };
+            return new SequenceExecutionJobWrapper<T, NativeListJob, NativeList<T>>(job, output);
+        }
+
+        public struct NativeListJob : IJob
+        {
+            public ValueSequence<T, TQuery> Sequence;
+
+            [WriteOnly]
+            public NativeList<T> Output;
+
+            public void Execute()
+            {
+                Sequence.ToNativeList(Output);
+            }
+        }
+    }
+}
