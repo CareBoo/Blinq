135b5323dd4d6bcd9e5b301e9c67c7ac8b08e529
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-08 23:22:43 -0700

    test: :white_check_mark: Update Select Test to Burst Compiled job

--- Packages/com.careboo.blinq/CareBoo.Blinq.PerformanceTests/SelectTest.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq.PerformanceTests/SelectTest.cs
@@ -2,20 +2,36 @@
 using Unity.PerformanceTesting;
 using Linq = System.Linq.Enumerable;
 using Blinq = CareBoo.Blinq.NativeArrayExtensions;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Collections;
+
+[BurstCompile]
+internal struct SelectJob : IJob
+{
+    [ReadOnly]
+    public NativeArray<int> Source;
+
+    public void Execute()
+    {
+        Blinq.Select<int, long, IntToLong>(ref Source).Execute();
+    }
+}
 
 internal class SelectTest : BaseBlinqPerformanceTest
 {
+
     [Test, Performance]
     [Category("Performance")]
     public void BlinqSelectNativeSequencePerformance()
     {
-        MeasureBlinq(() => Blinq.Select<int, long, IntToLong>(ref source)).Run();
+        MeasureBlinq(() => new SelectJob { Source = source }.Run()).Run();
     }
 
     [Test, Performance]
     [Category("Performance")]
     public void LinqSelectNativeSequencePerformance()
     {
-        MeasureLinq(() => Linq.Select(source, default(IntToLong).Invoke)).Run();
+        MeasureLinq(() => Linq.ToList(Linq.Select(source, default(IntToLong).Invoke))).Run();
     }
 }
