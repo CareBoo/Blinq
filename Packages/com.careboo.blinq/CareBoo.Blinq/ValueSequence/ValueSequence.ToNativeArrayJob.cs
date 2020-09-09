20d410b6ea86ff5c86483dc9abb589109b80ab6e
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-08 23:50:53 -0700

    feat: :sparkles: Add ToNativeArray and ToNativeArrayJob
    Fixes #31

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.ToNativeArrayJob.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace CareBoo.Blinq
+{
+    public partial struct ValueSequence<T, TQuery>
+        : IEnumerable<T>
+        where T : struct
+        where TQuery : struct, IQuery<T>
+    {
+        public NativeArrayJobWrapper ToNativeArrayJob(Allocator allocator)
+        {
+            var jobOutput = new NativeList<T>(allocator);
+            var job = new NativeListJob { Sequence = this, Output = jobOutput };
+            return new NativeArrayJobWrapper(job, jobOutput, allocator);
+        }
+
+        public struct NativeArrayJobWrapper
+        {
+            readonly NativeListJob job;
+            readonly NativeList<T> jobOutput;
+            readonly Allocator allocator;
+
+            public NativeArrayJobWrapper(NativeListJob job, NativeList<T> jobOutput, Allocator allocator)
+            {
+                this.job = job;
+                this.jobOutput = jobOutput;
+                this.allocator = allocator;
+            }
+
+            public NativeArrayJobHandle Schedule()
+            {
+                var scheduledJob = job.Schedule();
+                return new NativeArrayJobHandle(scheduledJob, jobOutput, allocator);
+            }
+
+            public NativeArray<T> Run()
+            {
+                job.Run();
+                var output = new NativeArray<T>(jobOutput, allocator);
+                jobOutput.Dispose();
+                return output;
+            }
+        }
+
+        public struct NativeArrayJobHandle
+        {
+            readonly JobHandle scheduledJob;
+            readonly NativeList<T> jobOutput;
+            readonly Allocator allocator;
+
+            public bool IsCompleted => scheduledJob.IsCompleted;
+
+            public NativeArrayJobHandle(JobHandle scheduledJob, NativeList<T> jobOutput, Allocator allocator)
+            {
+                this.scheduledJob = scheduledJob;
+                this.jobOutput = jobOutput;
+                this.allocator = allocator;
+            }
+
+            public NativeArray<T> Complete()
+            {
+                scheduledJob.Complete();
+                var output = new NativeArray<T>(jobOutput, allocator);
+                jobOutput.Dispose();
+                return output;
+            }
+        }
+    }
+}
