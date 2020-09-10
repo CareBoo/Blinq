20d574333aa7d3c3a9a7dd8e2187b987a55f8fa2
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-10 00:40:09 -0700

    feat: :sparkles: Implement DefaultIfEmpty
    Fixes #5

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.DefaultIfEmpty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class NativeArrayExtensions
+    {
+        public static ValueSequence<T, ValueSequence<T, NativeArraySequence<T>>.DefaultIfEmptySequence> DefaultIfEmpty<T>(
+            this ref NativeArray<T> source,
+            T defaultVal = default
+            )
+            where T : unmanaged, IEquatable<T>
+        {
+            return source.ToValueSequence().DefaultIfEmpty(defaultVal);
+        }
+    }
+}
---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.DefaultIfEmpty.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public partial struct ValueSequence<T, TSource>
+    {
+        public struct DefaultIfEmptySequence : ISequence<T>
+        {
+            public TSource Source;
+
+            public T Default;
+
+            public NativeList<T> Execute()
+            {
+                var sourceList = Source.Execute();
+                if (sourceList.Length == 0)
+                    sourceList.Add(Default);
+                return sourceList;
+            }
+        }
+
+        public ValueSequence<T, DefaultIfEmptySequence> DefaultIfEmpty(T defaultVal = default)
+        {
+            var newSequence = new DefaultIfEmptySequence { Source = source, Default = defaultVal };
+            return Create(newSequence);
+        }
+    }
+
+}
