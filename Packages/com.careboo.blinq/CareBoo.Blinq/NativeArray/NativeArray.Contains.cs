5e60029aeb26e0ca8791aa4574b5576ff22840a1
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-09 23:10:06 -0700

    feat: :sparkles: Implement Contains
    Add contains to valuesequence, nativearray, and tests
    
    Fixes #4

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Contains.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class NativeArrayExtensions
+    {
+        public static bool Contains<T>(this ref NativeArray<T> source, T item)
+            where T : unmanaged, IEquatable<T>
+        {
+            for (var i = 0; i < source.Length; i++)
+                if (source[i].Equals(item))
+                    return true;
+            return false;
+        }
+
+        public static bool Contains<T, TComparer>(this ref NativeArray<T> source, T item, TComparer comparer)
+            where T : unmanaged, IEquatable<T>
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            for (var i = 0; i < source.Length; i++)
+                if (comparer.Equals(source[i], item))
+                    return true;
+            return false;
+        }
+    }
+}
---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Contains.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CareBoo.Blinq
+{
+    public partial struct ValueSequence<T, TSource>
+    {
+        public bool Contains(T item)
+        {
+            var sourceList = Execute();
+            for (var i = 0; i < sourceList.Length; i++)
+                if (sourceList[i].Equals(item))
+                    return true;
+            return false;
+        }
+
+        public bool Contains<TComparer>(T item, TComparer comparer)
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            var sourceList = Execute();
+            for (var i = 0; i < sourceList.Length; i++)
+                if (comparer.Equals(sourceList[i], item))
+                    return true;
+            return false;
+        }
+    }
+}
