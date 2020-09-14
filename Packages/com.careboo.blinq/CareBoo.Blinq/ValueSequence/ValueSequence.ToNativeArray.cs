b47bf8a57c6112738581a399e1c041a13e72c02a
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-13 20:54:48 -0700

    feat: :sparkles: Reimpl ToNative* extension methods

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.ToNativeArray.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class ValueSequenceExtensions
+    {
+        public static NativeArray<T> ToNativeArray<T, TSource>(
+            this ref ValueSequence<T, TSource> source,
+            Allocator allocator
+            )
+            where T : struct
+            where TSource : struct, ISequence<T>
+        {
+            var list = source.Execute();
+            var result = new NativeArray<T>(list, allocator);
+            list.Dispose();
+            return result;
+        }
+    }
+}
