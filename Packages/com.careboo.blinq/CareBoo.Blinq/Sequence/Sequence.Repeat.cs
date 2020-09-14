3d7ce42cd32163f3d3a691fb19dca5ef0a38dfca
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-13 22:36:25 -0700

    feat: :sparkles: Implement Sequence.Repeat
    Fixes #21

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/Sequence/Sequence.Repeat.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static ValueSequence<T, RepeatSequence<T>> Repeat<T>(T element, int count)
+            where T : struct
+        {
+            var seq = new RepeatSequence<T> { Element = element, Count = count };
+            return new ValueSequence<T, RepeatSequence<T>>(seq);
+        }
+    }
+
+    public struct RepeatSequence<T> : ISequence<T>
+        where T : struct
+    {
+        public T Element;
+        public int Count;
+
+        public NativeList<T> Execute()
+        {
+            var list = new NativeList<T>(Count, Allocator.Temp);
+            for (var i = 0; i < Count; i++)
+                list.AddNoResize(Element);
+            return list;
+        }
+    }
+}
