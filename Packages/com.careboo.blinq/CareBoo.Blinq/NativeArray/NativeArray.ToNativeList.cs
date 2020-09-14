cf90b62e8aa5c6ea25dae2d241877db3ebc323bd
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-13 22:17:58 -0700

    feat: :sparkles: Implement Conversion for NativeArray

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.ToNativeList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class NativeArrayExtensions
+    {
+        public static NativeList<T> ToNativeList<T>(
+            this ref NativeArray<T> source,
+            Allocator allocator
+            )
+            where T : unmanaged, IEquatable<T>
+        {
+            var result = new NativeList<T>(source.Length, allocator);
+            result.CopyFrom(source);
+            return result;
+        }
+    }
+}
