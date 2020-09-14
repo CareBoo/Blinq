f8139aca883f8f000190a4e20c734a12263c5209
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-13 23:14:49 -0700

    feat: :sparkles: Implement First and FirstOrDefault
    Fixes #10

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq.Tests/FirstTest.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using Linq = System.Linq.Enumerable;
+using static Utils;
+using Unity.Collections;
+using CareBoo.Blinq;
+using static ValueFuncs;
+
+internal class FirstTest
+{
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayFirst([ArrayValues] int[] sourceArr)
+    {
+        var source = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.First(source));
+        var actual = ExceptionAndValue(() => source.First());
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayFirstPredicate([ArrayValues] int[] sourceArr)
+    {
+        var source = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.First(source, EqualsZero.Invoke));
+        var actual = ExceptionAndValue(() => source.First(EqualsZero));
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeSequenceFirst([ArrayValues] int[] sourceArr)
+    {
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.First(source));
+        var actual = ExceptionAndValue(() => source.First());
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeSequenceFirstPredicate([ArrayValues] int[] sourceArr)
+    {
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.First(source, EqualsZero.Invoke));
+        var actual = ExceptionAndValue(() => source.First(EqualsZero));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayFirstOrDefault([ArrayValues] int[] sourceArr)
+    {
+        var source = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.FirstOrDefault(source));
+        var actual = ExceptionAndValue(() => source.FirstOrDefault());
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayFirstOrDefaultPredicate([ArrayValues] int[] sourceArr)
+    {
+        var source = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.FirstOrDefault(source, EqualsZero.Invoke));
+        var actual = ExceptionAndValue(() => source.FirstOrDefault(EqualsZero));
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeSequenceFirstOrDefault([ArrayValues] int[] sourceArr)
+    {
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.FirstOrDefault(source));
+        var actual = ExceptionAndValue(() => source.FirstOrDefault());
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeSequenceFirstOrDefaultPredicate([ArrayValues] int[] sourceArr)
+    {
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.FirstOrDefault(source, EqualsZero.Invoke));
+        var actual = ExceptionAndValue(() => source.FirstOrDefault(EqualsZero));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+}
