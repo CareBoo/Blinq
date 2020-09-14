f63a2dfe7f32c790a54d1e3138559f0aaef2a4a3
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-13 22:06:27 -0700

    feat: :sparkles: Implement ElementAt and ElementAtOrDefault
    Fixes #7

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.ElementAt.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class NativeArrayExtensions
+    {
+        public static T ElementAt<T>(
+            this ref NativeArray<T> source,
+            int index
+            )
+            where T : struct
+        {
+            return source[index];
+        }
+
+        public static T ElementAtOrDefault<T>(
+            this ref NativeArray<T> source,
+            int index,
+            T defaultVal = default
+            )
+            where T : struct
+        {
+            if (index >= 0 && index < source.Length)
+                return source[index];
+            return defaultVal;
+        }
+    }
+}
