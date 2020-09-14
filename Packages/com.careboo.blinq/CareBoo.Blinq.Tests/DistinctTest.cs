f44d7a893493dca3b7477eae77a200a0fec55656
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-13 21:18:21 -0700

    feat: :sparkles: Implement Distinct
    Fixes #6

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq.Tests/DistinctTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Unity.Collections;
+using Blinq = CareBoo.Blinq.NativeArrayExtensions;
+using Linq = System.Linq.Enumerable;
+using static Utils;
+
+internal class DistinctTest
+{
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayDistinct([ArrayValues] int[] sourceArr)
+    {
+        var source = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.Distinct(source)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.Distinct(ref source)));
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+}
---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Distinct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class NativeArrayExtensions
+    {
+        public static ValueSequence<T, DistinctSequence<T, NativeArraySequence<T>>> Distinct<T>(
+            this ref NativeArray<T> source
+            )
+            where T : unmanaged, IEquatable<T>
+        {
+            var seq = source.ToValueSequence();
+            return seq.Distinct();
+        }
+    }
+}
