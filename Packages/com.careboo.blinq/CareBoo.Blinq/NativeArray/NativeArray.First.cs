da6bc535d42b37f3f6cb1754bb1093786d5c3474
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-13 23:14:49 -0700

    feat: :sparkles: Implement First and FirstOrDefault
    Fixes #10

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.First.cs
@@ -0,0 +1,59 @@
+﻿using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static T First<T, TPredicate>(
+            this ref NativeArray<T> source,
+            ValueFunc<T, bool>.Impl<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            for (var i = 0; i < source.Length; i++)
+            {
+                var val = source[i];
+                if (predicate.Invoke(val))
+                    return val;
+            }
+            throw Error.NoMatch();
+        }
+
+        public static T FirstOrDefault<T, TPredicate>(
+            this ref NativeArray<T> source,
+            ValueFunc<T, bool>.Impl<TPredicate> predicate,
+            T defaultVal = default
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            for (var i = 0; i < source.Length; i++)
+            {
+                var val = source[i];
+                if (predicate.Invoke(val))
+                    return val;
+            }
+            return defaultVal;
+        }
+
+        public static T First<T>(
+            this ref NativeArray<T> source
+            )
+            where T : struct
+        {
+            return source[0];
+        }
+
+        public static T FirstOrDefault<T>(
+            this ref NativeArray<T> source,
+            T defaultVal = default
+            )
+            where T : struct
+        {
+            if (source.Length == 0)
+                return defaultVal;
+            return source[0];
+        }
+    }
+}
---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/Sequence/Sequence.First.cs
@@ -0,0 +1,65 @@
+﻿namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static T First<T, TSource, TPredicate>(
+            this ref ValueSequence<T, TSource> source,
+            ValueFunc<T, bool>.Impl<TPredicate> predicate
+            )
+            where T : struct
+            where TSource : struct, ISequence<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var list = source.Execute();
+            for (var i = 0; i < list.Length; i++)
+            {
+                var val = list[i];
+                if (predicate.Invoke(val))
+                    return val;
+            }
+            throw Error.NoMatch();
+        }
+
+        public static T FirstOrDefault<T, TSource, TPredicate>(
+            this ref ValueSequence<T, TSource> source,
+            ValueFunc<T, bool>.Impl<TPredicate> predicate,
+            T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var list = source.Execute();
+            for (var i = 0; i < list.Length; i++)
+            {
+                var val = list[i];
+                if (predicate.Invoke(val))
+                    return val;
+            }
+            return defaultVal;
+        }
+
+        public static T First<T, TSource>(
+            this ref ValueSequence<T, TSource> source
+            )
+            where T : struct
+            where TSource : struct, ISequence<T>
+        {
+            var list = source.Execute();
+            return list[0];
+        }
+
+        public static T FirstOrDefault<T, TSource>(
+            this ref ValueSequence<T, TSource> source,
+            T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T>
+        {
+            var list = source.Execute();
+            if (list.Length == 0)
+                return defaultVal;
+            return list[0];
+        }
+    }
+}
