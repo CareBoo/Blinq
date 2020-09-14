ed2f9e15f5889ad3bde20aa211f2c03e308a9810
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-13 22:35:45 -0700

    feat: :sparkles: Implement Sequence.Empty
    Fixes #8

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/Sequence/Sequence.Empty.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static ValueSequence<T, EmptySequence<T>> Empty<T>()
+            where T : struct
+        {
+            var seq = new EmptySequence<T>();
+            return new ValueSequence<T, EmptySequence<T>>(seq);
+        }
+    }
+
+    public struct EmptySequence<T> : ISequence<T>
+        where T : struct
+    {
+        public NativeList<T> Execute()
+        {
+            return new NativeList<T>(Allocator.Temp);
+        }
+    }
+}
