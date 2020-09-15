41b4f743dc39ac4cbe2da37a19e3945b381190d3
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 18:52:32 -0700

    feat: :sparkles: Implement Prepend
    Fixes #19

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq.Tests/PrependTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Linq = System.Linq.Enumerable;
+using static Utils;
+using Unity.Collections;
+using Blinq = CareBoo.Blinq.Sequence;
+using CareBoo.Blinq;
+
+internal class PrependTest
+{
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayPrepend([ArrayValues] int[] sourceArr)
+    {
+        int item = 1;
+        var source = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.Prepend(source, item)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.Prepend(ref source, item)));
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqValueSequencePrepend([ArrayValues] int[] sourceArr)
+    {
+        int item = 1;
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.Prepend(source, item)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.Prepend(source, item)));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+}
