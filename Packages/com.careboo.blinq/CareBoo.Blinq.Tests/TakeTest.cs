f2b9a9fa3564057fc0e41a5b7a5d3bc122cc9997
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 18:53:43 -0700

    feat: :sparkles: Implement Take and TakeWhile
    Fixes #28

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq.Tests/TakeTest.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using Linq = System.Linq.Enumerable;
+using static Utils;
+using static ValueFuncs;
+using Unity.Collections;
+using Blinq = CareBoo.Blinq.Sequence;
+using CareBoo.Blinq;
+
+internal class TakeTest
+{
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayTake([ArrayValues] int[] sourceArr)
+    {
+        var count = 5;
+        var source = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.Take(source, count)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.Take(ref source, count)));
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayTakeWhile([ArrayValues] int[] sourceArr)
+    {
+        var source = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.TakeWhile(source, EqualToIndex.Invoke)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.TakeWhile(ref source, EqualToIndex)));
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqValueSequenceTake([ArrayValues] int[] sourceArr)
+    {
+        var count = 5;
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.Take(source, count)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.Take(source, count)));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqValueSequenceTakeWhile([ArrayValues] int[] sourceArr)
+    {
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.TakeWhile(source, EqualToIndex.Invoke)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.TakeWhile(source, EqualToIndex)));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+}
