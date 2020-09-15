cbd228fb21a1c9c4b6129eebd1119302d2801f4c
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 19:01:34 -0700

    feat: :sparkles: Implement SequenceEqual
    Fixes #24

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq.Tests/SequenceEqualTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Linq = System.Linq.Enumerable;
+using static Utils;
+using Unity.Collections;
+using Blinq = CareBoo.Blinq.Sequence;
+using CareBoo.Blinq;
+
+internal class SequenceEqualTest
+{
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArraySequenceEqualArray([ArrayValues] int[] sourceArr, [ArrayValues] int[] secondArr)
+    {
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var secondNativeArr = new NativeArray<int>(secondArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.SequenceEqual(sourceNativeArr, secondNativeArr));
+        var actual = ExceptionAndValue(() => Blinq.SequenceEqual(ref sourceNativeArr, secondNativeArr));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+        secondNativeArr.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqValueSequenceEqualArray([ArrayValues] int[] sourceArr, [ArrayValues] int[] secondArr)
+    {
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var secondNativeArr = new NativeArray<int>(secondArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.SequenceEqual(source, secondNativeArr));
+        var actual = ExceptionAndValue(() => Blinq.SequenceEqual(source, secondNativeArr));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+        secondNativeArr.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArraySequenceEqualSequence([ArrayValues] int[] sourceArr, [ArrayValues] int[] secondArr)
+    {
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var secondNativeArr = new NativeArray<int>(secondArr, Allocator.Persistent);
+        var second = secondNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.SequenceEqual(sourceNativeArr, second));
+        var actual = ExceptionAndValue(() => Blinq.SequenceEqual(ref sourceNativeArr, second));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+        secondNativeArr.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqValueSequenceEqualSequence([ArrayValues] int[] sourceArr, [ArrayValues] int[] secondArr)
+    {
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var secondNativeArr = new NativeArray<int>(secondArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var second = secondNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.SequenceEqual(source, second));
+        var actual = ExceptionAndValue(() => Blinq.SequenceEqual(source, second));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+        secondNativeArr.Dispose();
+    }
+}
---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.SequenceEqual.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static bool SequenceEqual<T, TSecond>(
+            this ref NativeArray<T> source,
+            ValueSequence<T, TSecond> second
+            )
+            where T : struct, IEquatable<T>
+            where TSecond : struct, ISequence<T>
+        {
+            return source.ToValueSequence().SequenceEqual(second);
+        }
+
+        public static bool SequenceEqual<T, TSecond, TComparer>(
+            this ref NativeArray<T> source,
+            ValueSequence<T, TSecond> second,
+            TComparer comparer
+            )
+            where T : struct, IEquatable<T>
+            where TSecond : struct, ISequence<T>
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            return source.ToValueSequence().SequenceEqual(second, comparer);
+        }
+
+        public static bool SequenceEqual<T>(
+            this ref NativeArray<T> source,
+            NativeArray<T> second
+            )
+            where T : struct, IEquatable<T>
+        {
+            return source.ArraysEqual(second);
+        }
+
+        public static bool SequenceEqual<T, TComparer>(
+            this ref NativeArray<T> source,
+            NativeArray<T> second,
+            TComparer comparer
+            )
+            where T : struct, IEquatable<T>
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            if (source.Length != second.Length)
+                return false;
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (!comparer.Equals(source[i], second[i]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
