852db69f4257fa186da0676948fcdf14e2cb07df
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 18:55:52 -0700

    feat: :sparkles: Implement Reverse
    Fixes #22

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Reverse.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static ValueSequence<T, ReverseSequence<T, NativeArraySequence<T>>> Reverse<T>(
+            this ref NativeArray<T> source
+            )
+            where T : struct
+        {
+            return source.ToValueSequence().Reverse();
+        }
+    }
+}
