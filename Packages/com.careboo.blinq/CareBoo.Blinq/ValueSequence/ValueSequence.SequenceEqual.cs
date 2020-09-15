706d21af4bba1d78b02c57637b2e70f74e600d8e
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 19:01:34 -0700

    feat: :sparkles: Implement SequenceEqual
    Fixes #24

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.SequenceEqual.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static bool SequenceEqual<T, TSource, TSecond>(
+            this ValueSequence<T, TSource> source,
+            ValueSequence<T, TSecond> second
+            )
+            where T : struct, IEquatable<T>
+            where TSource : struct, ISequence<T>
+            where TSecond : struct, ISequence<T>
+        {
+            using (var sourceList = source.Execute())
+            using (var secondList = second.Execute())
+            {
+                return sourceList.AsArray().ArraysEqual(secondList);
+            }
+        }
+
+        public static bool SequenceEqual<T, TSource, TSecond, TComparer>(
+            this ValueSequence<T, TSource> source,
+            ValueSequence<T, TSecond> second,
+            TComparer comparer
+            )
+            where T : struct, IEquatable<T>
+            where TSource : struct, ISequence<T>
+            where TSecond : struct, ISequence<T>
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            using (var sourceList = source.Execute())
+            using (var secondList = second.Execute())
+            {
+                if (sourceList.Length != secondList.Length)
+                    return false;
+                for (var i = 0; i < sourceList.Length; i++)
+                {
+                    if (!comparer.Equals(sourceList[i], secondList[i]))
+                        return false;
+                }
+                return true;
+            }
+        }
+
+        public static bool SequenceEqual<T, TSource>(
+            this ValueSequence<T, TSource> source,
+            NativeArray<T> second
+            )
+            where T : struct, IEquatable<T>
+            where TSource : struct, ISequence<T>
+        {
+            using (var sourceList = source.Execute())
+            {
+                return sourceList.AsArray().ArraysEqual(second);
+            }
+        }
+
+        public static bool SequenceEqual<T, TSource, TComparer>(
+            this ValueSequence<T, TSource> source,
+            NativeArray<T> second,
+            TComparer comparer
+            )
+            where T : struct, IEquatable<T>
+            where TSource : struct, ISequence<T>
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            using (var sourceList = source.Execute())
+            {
+                if (sourceList.Length != second.Length)
+                    return false;
+                for (var i = 0; i < sourceList.Length; i++)
+                {
+                    if (!comparer.Equals(sourceList[i], second[i]))
+                        return false;
+                }
+                return true;
+            }
+        }
+
+    }
+}
