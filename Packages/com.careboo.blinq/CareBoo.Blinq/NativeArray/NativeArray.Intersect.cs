4d092c314b972ffe131d3c3803c9708e6f9986fa
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 22:20:50 -0700

    feat: :sparkles: Implement Intersect
    Fixes #13

--- Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Intersect.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Intersect.cs
@@ -5,23 +5,23 @@ namespace CareBoo.Blinq
 {
     public static partial class Sequence
     {
-        public static ValueSequence<T, UnionSequence<T, NativeArraySequence<T>, TSecond>> Union<T, TSecond>(
+        public static ValueSequence<T, IntersectSequence<T, NativeArraySequence<T>, TSecond>> Intersect<T, TSecond>(
            this ref NativeArray<T> source,
            ValueSequence<T, TSecond> second
            )
            where T : unmanaged, IEquatable<T>
            where TSecond : struct, ISequence<T>
         {
-            return source.ToValueSequence().Union(second);
+            return source.ToValueSequence().Intersect(second);
         }
 
-        public static ValueSequence<T, UnionSequence<T, NativeArraySequence<T>, NativeArraySequence<T>>> Union<T>(
+        public static ValueSequence<T, IntersectSequence<T, NativeArraySequence<T>, NativeArraySequence<T>>> Intersect<T>(
            this ref NativeArray<T> source,
            NativeArray<T> second
            )
            where T : unmanaged, IEquatable<T>
         {
-            return source.ToValueSequence().Union(second);
+            return source.ToValueSequence().Intersect(second);
         }
     }
 }
---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Intersect.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static ValueSequence<T, IntersectSequence<T, TSource, TSecond>> Intersect<T, TSource, TSecond>(
+            this ValueSequence<T, TSource> source,
+            ValueSequence<T, TSecond> second
+            )
+            where T : unmanaged, IEquatable<T>
+            where TSource : struct, ISequence<T>
+            where TSecond : struct, ISequence<T>
+        {
+            var seq = new IntersectSequence<T, TSource, TSecond> { Source = source.Source, Second = second.Source };
+            return new ValueSequence<T, IntersectSequence<T, TSource, TSecond>>(seq);
+        }
+
+        public static ValueSequence<T, IntersectSequence<T, TSource, NativeArraySequence<T>>> Intersect<T, TSource>(
+            this ValueSequence<T, TSource> source,
+            NativeArray<T> second
+            )
+            where T : unmanaged, IEquatable<T>
+            where TSource : struct, ISequence<T>
+        {
+            return source.Intersect(second.ToValueSequence());
+        }
+    }
+
+    public struct IntersectSequence<T, TSource, TSecond> : ISequence<T>
+        where T : unmanaged, IEquatable<T>
+        where TSource : struct, ISequence<T>
+        where TSecond : struct, ISequence<T>
+    {
+        public TSource Source;
+        public TSecond Second;
+
+        public NativeList<T> Execute()
+        {
+            using (var sourceList = Source.Execute())
+            using (var secondList = Second.Execute())
+            using (var sourceSet = new NativeHashSet<T>(sourceList.Length, Allocator.Temp))
+            using (var secondSet = new NativeHashSet<T>(secondList.Length, Allocator.Temp))
+            {
+                var result = new NativeList<T>(secondList.Length, Allocator.Temp);
+                for (var i = 0; i < sourceList.Length; i++)
+                    sourceSet.Add(sourceList[i]);
+                for (var i = 0; i < secondList.Length; i++)
+                    if (sourceSet.Contains(secondList[i]) && secondSet.Add(secondList[i]))
+                        result.AddNoResize(secondList[i]);
+                return result;
+            }
+        }
+    }
+}
