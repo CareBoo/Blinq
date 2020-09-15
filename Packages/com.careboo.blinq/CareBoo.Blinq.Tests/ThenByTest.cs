09644f1b10abb58bac72f78b6b9884ae1873b71c
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 19:26:54 -0700

    chore: :poop: Start working on ThenBy

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq.Tests/ThenByTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using Linq = System.Linq.Enumerable;
+using static Utils;
+using static ValueFuncs;
+using Unity.Collections;
+using Blinq = CareBoo.Blinq.Sequence;
+using CareBoo.Blinq;
+
+internal class ThenByTest
+{
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayThenBy([OrderValues] Order[] sourceArr)
+    {
+        var source = new NativeArray<Order>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.ThenBy(Linq.OrderBy(source, SelectSecond.Invoke), SelectFirst.Invoke)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.ThenBy(Blinq.OrderBy(ref source, SelectSecond), SelectFirst)));
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqValueSequenceThenBy([OrderValues] Order[] sourceArr)
+    {
+        var sourceNativeArr = new NativeArray<Order>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.ThenBy(Linq.OrderBy(source, SelectSecond.Invoke), SelectFirst.Invoke)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.ThenBy(Blinq.OrderBy(source, SelectSecond), SelectFirst)));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayThenByDescending([OrderValues] Order[] sourceArr)
+    {
+        var source = new NativeArray<Order>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.ThenByDescending(Linq.OrderBy(source, SelectFirst.Invoke), SelectSecond.Invoke)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.ThenByDescending(Blinq.OrderBy(ref source, SelectFirst), SelectSecond)));
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqValueSequenceThenByDescending([OrderValues] Order[] sourceArr)
+    {
+        var sourceNativeArr = new NativeArray<Order>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.ThenByDescending(Linq.OrderBy(source, SelectFirst.Invoke), SelectSecond.Invoke)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.ThenByDescending(Blinq.OrderBy(source, SelectFirst), SelectSecond)));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+}
---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.ThenBy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CareBoo.Blinq
+{
+    // TODO: Need a custom thenby sequence that can leverage a previous orderby being called
+    public static partial class Sequence
+    {
+        public static ValueSequence<T, OrderBySequence<T, TSource, TKey, TKeySelector>> ThenBy<T, TSource, TKey, TKeySelector>(
+            this ValueSequence<T, TSource> source,
+            ValueFunc<T, TKey>.Impl<TKeySelector> keySelector
+            )
+            where T : struct
+            where TSource : struct, ISequence<T>
+            where TKey : struct, IComparable<TKey>
+            where TKeySelector : struct, IFunc<T, TKey>
+        {
+            return source.OrderBy(keySelector);
+        }
+
+        public static ValueSequence<T, OrderByComparerSequence<T, TSource, TKey, TKeySelector, TComparer>> ThenBy<T, TSource, TKey, TKeySelector, TComparer>(
+            this ValueSequence<T, TSource> source,
+            ValueFunc<T, TKey>.Impl<TKeySelector> keySelector,
+            TComparer comparer
+            )
+            where T : struct
+            where TSource : struct, ISequence<T>
+            where TKey : struct
+            where TKeySelector : struct, IFunc<T, TKey>
+            where TComparer : struct, IComparer<TKey>
+        {
+            return source.OrderBy(keySelector, comparer);
+        }
+    }
+}
