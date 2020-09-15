8ba0fed6ef73ef15a000c2b1d857dc0473d09731
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 19:26:54 -0700

    chore: :poop: Start working on ThenBy

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.ThenByDescending.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static ValueSequence<T, OrderByDescendingSequence<T, NativeArraySequence<T>, TKey, TKeySelector>> ThenByDescending<T, TKey, TKeySelector>(
+            this ref NativeArray<T> source,
+            ValueFunc<T, TKey>.Impl<TKeySelector> keySelector
+            )
+            where T : struct
+            where TKey : struct, IComparable<TKey>
+            where TKeySelector : struct, IFunc<T, TKey>
+        {
+            return source.ToValueSequence().OrderByDescending(keySelector);
+        }
+
+        public static ValueSequence<T, OrderByComparerDescendingSequence<T, NativeArraySequence<T>, TKey, TKeySelector, TComparer>> ThenByDescending<T, TKey, TKeySelector, TComparer>(
+            this ref NativeArray<T> source,
+            ValueFunc<T, TKey>.Impl<TKeySelector> keySelector,
+            TComparer comparer
+            )
+            where T : struct
+            where TKey : struct
+            where TKeySelector : struct, IFunc<T, TKey>
+            where TComparer : struct, IComparer<TKey>
+        {
+            return source.ToValueSequence().OrderByDescending(keySelector, comparer);
+        }
+    }
+}
---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequenced.ThenByDescending.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static ValueSequence<T, OrderByDescendingSequence<T, TSource, TKey, TKeySelector>> ThenByDescending<T, TSource, TKey, TKeySelector>(
+            this ValueSequence<T, TSource> source,
+            ValueFunc<T, TKey>.Impl<TKeySelector> keySelector
+            )
+            where T : struct
+            where TSource : struct, ISequence<T>
+            where TKey : struct, IComparable<TKey>
+            where TKeySelector : struct, IFunc<T, TKey>
+        {
+            return source.OrderByDescending(keySelector);
+        }
+
+        public static ValueSequence<T, OrderByComparerDescendingSequence<T, TSource, TKey, TKeySelector, TComparer>> ThenByDescending<T, TSource, TKey, TKeySelector, TComparer>(
+            this ValueSequence<T, TSource> source,
+            ValueFunc<T, TKey>.Impl<TKeySelector> keySelector,
+            TComparer comparer
+            )
+            where T : struct
+            where TSource : struct, ISequence<T>
+            where TKey : struct
+            where TKeySelector : struct, IFunc<T, TKey>
+            where TComparer : struct, IComparer<TKey>
+        {
+            return source.OrderByDescending(keySelector, comparer);
+        }
+    }
+}
