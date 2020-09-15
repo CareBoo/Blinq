610dfff0d0598d7bb68077655b5f01f23f9caa8f
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 18:52:54 -0700

    test: :white_check_mark: Add tests for Concat and Append

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq.Tests/AppendTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Linq = System.Linq.Enumerable;
+using static Utils;
+using Unity.Collections;
+using Blinq = CareBoo.Blinq.Sequence;
+using CareBoo.Blinq;
+
+internal class AppendTest
+{
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayAppend([ArrayValues] int[] sourceArr)
+    {
+        int item = 1;
+        var source = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.Append(source, item)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.Append(ref source, item)));
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqValueSequenceAppend([ArrayValues] int[] sourceArr)
+    {
+        int item = 1;
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.Append(source, item)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.Append(source, item)));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+}
