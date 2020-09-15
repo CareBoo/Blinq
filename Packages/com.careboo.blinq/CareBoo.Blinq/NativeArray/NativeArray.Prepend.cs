35ec7226839b8a7f3ac41464b8bc0d1d88578a80
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 18:52:32 -0700

    feat: :sparkles: Implement Prepend
    Fixes #19

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Prepend.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static ValueSequence<T, PrependSequence<T, NativeArraySequence<T>>> Prepend<T>(
+            this ref NativeArray<T> source,
+            T item
+            )
+            where T : struct
+        {
+            return source.ToValueSequence().Prepend(item);
+        }
+    }
+}
