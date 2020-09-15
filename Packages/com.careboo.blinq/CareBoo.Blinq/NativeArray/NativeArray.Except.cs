2ed72b6055d4994769da09e1a14adc7b601fa789
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 23:40:41 -0700

    feat: :sparkles: Implement Except
    Fixes #9

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Except.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static ValueSequence<T, ExceptSequence<T, NativeArraySequence<T>, TSecond>> Except<T, TSecond>(
+            this ref NativeArray<T> source,
+            ValueSequence<T, TSecond> second
+            )
+            where T : unmanaged, IEquatable<T>
+            where TSecond : struct, ISequence<T>
+        {
+            return source.ToValueSequence().Except(second);
+        }
+
+        public static ValueSequence<T, ExceptSequence<T, NativeArraySequence<T>, NativeArraySequence<T>>> Except<T>(
+            this ref NativeArray<T> source,
+            NativeArray<T> second
+            )
+            where T : unmanaged, IEquatable<T>
+        {
+            return source.ToValueSequence().Except(second);
+        }
+    }
+}
