204fab58d9b4483a72a00b6e760d16f53b1fd414
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 18:55:52 -0700

    feat: :sparkles: Implement Reverse
    Fixes #22

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq.Tests/ReverseTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Linq = System.Linq.Enumerable;
+using static Utils;
+using static ValueFuncs;
+using Unity.Collections;
+using Blinq = CareBoo.Blinq.Sequence;
+using CareBoo.Blinq;
+
+internal class ReverseTest
+{
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqNativeArrayReverse([ArrayValues] int[] sourceArr)
+    {
+        var source = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.Reverse(source)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.Reverse(ref source)));
+        AssertAreEqual(expected, actual);
+        source.Dispose();
+    }
+
+    [Test, Parallelizable]
+    public void BlinqShouldEqualLinqValueSequenceReverse([ArrayValues] int[] sourceArr)
+    {
+        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
+        var source = sourceNativeArr.ToValueSequence();
+        var expected = ExceptionAndValue(() => Linq.ToArray(Linq.Reverse(source)));
+        var actual = ExceptionAndValue(() => Linq.ToArray(Blinq.Reverse(source)));
+        AssertAreEqual(expected, actual);
+        sourceNativeArr.Dispose();
+    }
+}
---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Reverse.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static ValueSequence<T, ReverseSequence<T, TSource>> Reverse<T, TSource>(
+            this ValueSequence<T, TSource> source
+            )
+            where T : struct
+            where TSource : struct, ISequence<T>
+        {
+            var seq = new ReverseSequence<T, TSource> { Source = source.Source };
+            return new ValueSequence<T, ReverseSequence<T, TSource>>(seq);
+        }
+    }
+
+    public struct ReverseSequence<T, TSource> : ISequence<T>
+        where T : struct
+        where TSource : struct, ISequence<T>
+    {
+        public TSource Source;
+
+        public NativeList<T> Execute()
+        {
+            var list = Source.Execute();
+            for (var i = 0; i < list.Length / 2; i++)
+            {
+                var swap = list.Length - 1 - i;
+                var tmp = list[i];
+                list[i] = list[swap];
+                list[swap] = tmp;
+            }
+            return list;
+        }
+    }
+}
