4d4e36e16ae2d723aab621eae940b9534ffd5a49
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-14 17:07:12 -0700

    fix: :bug: Fix dispose in Count

--- Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Count.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Count.cs
@@ -10,20 +10,22 @@
             where TSource : struct, ISequence<T>
             where TPredicate : struct, IFunc<T, bool>
         {
-            var count = 0;
-            var sourceList = source.Execute();
-            for (var i = 0; i < sourceList.Length; i++)
-                if (predicate.Invoke(sourceList[i]))
-                    count++;
-            return count;
+            using (var sourceList = source.Execute())
+            {
+                var count = 0;
+                for (var i = 0; i < sourceList.Length; i++)
+                    if (predicate.Invoke(sourceList[i]))
+                        count++;
+                return count;
+            }
         }
 
         public static int Count<T, TSource>(this ref ValueSequence<T, TSource> source)
             where T : struct
             where TSource : struct, ISequence<T>
         {
-            var sourceList = source.Execute();
-            return sourceList.Length;
+            using (var sourceList = source.Execute())
+                return sourceList.Length;
         }
     }
 }
