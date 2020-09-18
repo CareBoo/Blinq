56ae0e59b7038cae6741f90ba76ca6e07cf4ffaf
Jason Bou Kheir <jason.boukheir@pm.me>
2020-09-17 21:20:41 -0700

    feat: :sparkles: Implement MaxMin
    Fixes #17

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.MaxMin/NativeArray.MaxMin.cs
@@ -0,0 +1,74 @@
+using System;
+using Unity.Collections;
+
+namespace CareBoo.Blinq
+{
+    public static partial class Sequence
+    {
+        public static TResult Max<T, TResult, TSelector>(
+            this ref NativeArray<T> source,
+            ValueFunc<T, TResult>.Impl<TSelector> selector
+            )
+            where T : struct
+            where TResult : struct, IComparable<TResult>
+            where TSelector : struct, IFunc<T, TResult>
+        {
+            if (source.Length == 0) throw Error.NoElements();
+            var max = selector.Invoke(source[0]);
+            for (var i = 1; i < source.Length; i++)
+            {
+                var val = selector.Invoke(source[i]);
+                if (val.CompareTo(max) > 0)
+                    max = val;
+            }
+            return max;
+        }
+
+        public static T Max<T>(ref NativeArray<T> source)
+            where T : struct, IComparable<T>
+        {
+            if (source.Length == 0) throw Error.NoElements();
+            var max = source[0];
+            for (var i = 1; i < source.Length; i++)
+            {
+                var val = source[i];
+                if (val.CompareTo(max) > 0)
+                    max = val;
+            }
+            return max;
+        }
+
+        public static TResult Min<T, TResult, TSelector>(
+            ref NativeArray<T> source,
+            ValueFunc<T, TResult>.Impl<TSelector> selector
+            )
+            where T : struct
+            where TResult : struct, IComparable<TResult>
+            where TSelector : struct, IFunc<T, TResult>
+        {
+            if (source.Length == 0) throw Error.NoElements();
+            var min = selector.Invoke(source[0]);
+            for (var i = 1; i < source.Length; i++)
+            {
+                var val = selector.Invoke(source[i]);
+                if (val.CompareTo(min) < 0)
+                    min = val;
+            }
+            return min;
+        }
+
+        public static T Min<T>(ref NativeArray<T> source)
+            where T : struct, IComparable<T>
+        {
+            if (source.Length == 0) throw Error.NoElements();
+            var min = source[0];
+            for (var i = 1; i < source.Length; i++)
+            {
+                var val = source[i];
+                if (val.CompareTo(min) < 0)
+                    min = val;
+            }
+            return min;
+        }
+    }
+}
