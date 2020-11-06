9942e1c06748c4fd0cdc29d906b6eed7fc69c2ab
Jason Bou Kheir <jason.boukheir@pm.me>
2020-11-05 19:51:29 -0800

    chore: add groupby performance

---
+++ Packages/com.careboo.blinq/CareBoo.Blinq.PerformanceTests/GroupByPerformanceTest.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+using Unity.PerformanceTesting;
+using Linq = System.Linq.Enumerable;
+using Blinq = CareBoo.Blinq.Sequence;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Collections;
+using static ValueFuncs;
+using CareBoo.Burst.Delegates;
+using System.Collections.Generic;
+
+[BurstCompile]
+internal struct GroupByPerformanceJob<TOuterKeySelector, TInnerKeySelector, TResultSelector> : IJob
+    where TOuterKeySelector : struct, IFunc<JoinA, int>
+    where TInnerKeySelector : struct, IFunc<JoinB, int>
+    where TResultSelector : struct, IFunc<JoinA, NativeArray<JoinB>, int>
+{
+    [ReadOnly]
+    public NativeArray<JoinA> Outer;
+    [ReadOnly]
+    public NativeArray<JoinB> Inner;
+
+    public ValueFunc<JoinA, int>.Struct<TOuterKeySelector> OuterKeySelector;
+    public ValueFunc<JoinB, int>.Struct<TInnerKeySelector> InnerKeySelector;
+    public ValueFunc<JoinA, NativeArray<JoinB>, int>.Struct<TResultSelector> ResultSelector;
+
+    public void Execute()
+    {
+        Blinq.GroupJoin(Outer, Inner, OuterKeySelector, InnerKeySelector, ResultSelector).Execute();
+    }
+}
+
+internal class GroupByPerformanceTest : BaseBlinqPerformanceTest
+{
+    public override void SetUpSource()
+    {
+    }
+
+    public override void TearDownSource()
+    {
+    }
+
+    [Test, Performance, Category("Performance")]
+    public void GroupByPerformance()
+    {
+        var outer = new NativeArray<JoinA>(1000, Allocator.Persistent);
+        for (var i = 0; i < outer.Length; i++)
+            outer[i] = new JoinA { Id = i, Val = (char)(i % 26 + (int)'a') };
+        var inner = new NativeArray<JoinB>(10000, Allocator.Persistent);
+        for (var i = 0; i < inner.Length; i++)
+            inner[i] = new JoinB { Id = i % 1000, Val = (char)(i % 26 + (int)'A') };
+
+        int GroupJoinLinqSelector(JoinA o, IEnumerable<JoinB> b)
+        {
+            return Linq.Count(b);
+        }
+
+        MakeMeasurement(() => new GroupByPerformanceJob<Functions.JoinAKeySelector, Functions.JoinBKeySelector, Functions.GroupJoinABSelector> { Outer = outer, Inner = inner }.Run(), "Blinq").Run();
+        MakeMeasurement(() => Linq.ToList(Linq.GroupJoin<JoinA, JoinB, int, int>(outer, inner, JoinAKeySelector.Invoke, JoinBKeySelector.Invoke, GroupJoinLinqSelector)), "Linq").Run();
+        outer.Dispose();
+        inner.Dispose();
+    }
+}
