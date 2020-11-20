e02060293c9bc3e0881c41ba635dc125c0461a39
Jason Bou Kheir <jason.boukheir@pm.me>
2020-11-19 23:48:03 -0800

    feat: :sparkles: Add `IGrouping` GroupBy implementation
    Fixes #56

--- Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.GroupBy.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.GroupBy.cs
@@ -10,7 +10,7 @@ namespace CareBoo.Blinq
             this in NativeArray<T> source,
             in ValueFunc<T, TKey>.Struct<TKeySelector> keySelector,
             in ValueFunc<T, TElement>.Struct<TElementSelector> elementSelector,
-            in ValueFunc<TKey, NativeMultiHashMap<TKey, TElement>, TResult>.Struct<TResultSelector> resultSelector
+            in ValueFunc<TKey, ValueGroupingValues<TKey, TElement>, TResult>.Struct<TResultSelector> resultSelector
             )
             where T : struct
             where TKey : unmanaged, IEquatable<TKey>
@@ -18,7 +18,7 @@ namespace CareBoo.Blinq
             where TElement : struct
             where TElementSelector : struct, IFunc<T, TElement>
             where TResult : struct
-            where TResultSelector : struct, IFunc<TKey, NativeMultiHashMap<TKey, TElement>, TResult>
+            where TResultSelector : struct, IFunc<TKey, ValueGroupingValues<TKey, TElement>, TResult>
         {
             return source.ToValueSequence().GroupBy(keySelector, elementSelector, resultSelector);
         }
@@ -26,15 +26,40 @@ namespace CareBoo.Blinq
         public static ValueSequence<TResult, GroupBySequence<T, NativeArraySequence<T>, TKey, TKeySelector, T, SameSelector<T>, TResult, TResultSelector>> GroupBy<T, TKey, TKeySelector, TResult, TResultSelector>(
             this in NativeArray<T> source,
             in ValueFunc<T, TKey>.Struct<TKeySelector> keySelector,
-            in ValueFunc<TKey, NativeMultiHashMap<TKey, T>, TResult>.Struct<TResultSelector> resultSelector
+            in ValueFunc<TKey, ValueGroupingValues<TKey, T>, TResult>.Struct<TResultSelector> resultSelector
             )
             where T : struct
             where TKey : unmanaged, IEquatable<TKey>
             where TKeySelector : struct, IFunc<T, TKey>
             where TResult : struct
-            where TResultSelector : struct, IFunc<TKey, NativeMultiHashMap<TKey, T>, TResult>
+            where TResultSelector : struct, IFunc<TKey, ValueGroupingValues<TKey, T>, TResult>
         {
             return source.ToValueSequence().GroupBy(keySelector, resultSelector);
+        }
+
+        public static ValueSequence<ValueGrouping<TKey, TElement>, GroupBySequence<T, NativeArraySequence<T>, TKey, TKeySelector, TElement, TElementSelector, ValueGrouping<TKey, TElement>, GroupSelector<TKey, TElement>>> GroupBy<T, TKey, TKeySelector, TElement, TElementSelector>(
+            this in NativeArray<T> source,
+            ValueFunc<T, TKey>.Struct<TKeySelector> keySelector,
+            ValueFunc<T, TElement>.Struct<TElementSelector> elementSelector
+            )
+            where T : struct
+            where TKey : unmanaged, IEquatable<TKey>
+            where TKeySelector : struct, IFunc<T, TKey>
+            where TElement : struct
+            where TElementSelector : struct, IFunc<T, TElement>
+        {
+            return source.ToValueSequence().GroupBy(keySelector, elementSelector);
+        }
+
+        public static ValueSequence<ValueGrouping<TKey, T>, GroupBySequence<T, NativeArraySequence<T>, TKey, TKeySelector, T, SameSelector<T>, ValueGrouping<TKey, T>, GroupSelector<TKey, T>>> GroupBy<T, TKey, TKeySelector>(
+            this NativeArray<T> source,
+            ValueFunc<T, TKey>.Struct<TKeySelector> keySelector
+            )
+            where T : struct
+            where TKey : unmanaged, IEquatable<TKey>
+            where TKeySelector : struct, IFunc<T, TKey>
+        {
+            return source.ToValueSequence().GroupBy(keySelector);
         }
     }
 }
