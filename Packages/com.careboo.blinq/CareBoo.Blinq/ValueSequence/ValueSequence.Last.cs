d650406b13a8e4c28a1fb112df35477b2981ff00
Jason Bou Kheir <jason.boukheir@pm.me>
2020-11-28 21:30:02 -0800

    feat: :sparkles: Implment RunLast and ScheduleLast
    Fixes #75

--- Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Last.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Last.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using CareBoo.Burst.Delegates;
 using Unity.Collections;
+using Unity.Jobs;
 
 namespace CareBoo.Blinq
 {
@@ -29,6 +30,73 @@ namespace CareBoo.Blinq
             throw Error.NoMatch();
         }
 
+        public struct LastFunc<T, TSource, TSourceEnumerator, TPredicate>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            public ValueFunc<T, bool>.Struct<TPredicate> Predicate;
+            public T Invoke(ValueSequence<T, TSource, TSourceEnumerator> arg0)
+            {
+                return arg0.Last(Predicate);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.Struct<LastFunc<T, TSource, TSourceEnumerator, TPredicate>> NewLastFunc<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = new LastFunc<T, TSource, TSourceEnumerator, TPredicate> { Predicate = predicate };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.New(func);
+        }
+
+        public static T RunLast<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var singleFunc = source.NewLastFunc(predicate);
+            return source.Run(source.NewLastFunc(predicate));
+        }
+
+        public static JobHandle ScheduleLast<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<T> output,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var singleFunc = source.NewLastFunc(predicate);
+            return source.Schedule(source.NewLastFunc(predicate), ref output);
+        }
+
+        public static JobHandle<T> ScheduleLast<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var singleFunc = source.NewLastFunc(predicate);
+            return source.Schedule(source.NewLastFunc(predicate));
+        }
+
         public static T LastOrDefault<T, TSource, TSourceEnumerator, TPredicate>(
             this in ValueSequence<T, TSource, TSourceEnumerator> source,
             ValueFunc<T, bool>.Struct<TPredicate> predicate,
@@ -53,6 +121,78 @@ namespace CareBoo.Blinq
             return defaultVal;
         }
 
+        public struct LastOrDefaultFunc<T, TSource, TSourceEnumerator, TPredicate>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            public ValueFunc<T, bool>.Struct<TPredicate> Predicate;
+            public T Default;
+            public T Invoke(ValueSequence<T, TSource, TSourceEnumerator> arg0)
+            {
+                return arg0.LastOrDefault(Predicate, in Default);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.Struct<LastOrDefaultFunc<T, TSource, TSourceEnumerator, TPredicate>> NewLastOrDefaultFunc<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = new LastOrDefaultFunc<T, TSource, TSourceEnumerator, TPredicate> { Predicate = predicate, Default = defaultVal };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.New(func);
+        }
+
+        public static T RunLastOrDefault<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = source.NewLastOrDefaultFunc(predicate, in defaultVal);
+            return source.Run(func);
+        }
+
+        public static JobHandle ScheduleLastOrDefault<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<T> output,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = source.NewLastOrDefaultFunc(predicate, in defaultVal);
+            return source.Schedule(func, ref output);
+        }
+
+        public static JobHandle<T> ScheduleLastOrDefault<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = source.NewLastOrDefaultFunc(predicate, in defaultVal);
+            return source.Schedule(func);
+        }
+
         public static T Last<T, TSource, TSourceEnumerator>(
             this in ValueSequence<T, TSource, TSourceEnumerator> source
             )
@@ -71,6 +211,63 @@ namespace CareBoo.Blinq
             return result;
         }
 
+        public struct LastFunc<T, TSource, TSourceEnumerator>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            public T Invoke(ValueSequence<T, TSource, TSourceEnumerator> arg0)
+            {
+                return arg0.Last();
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.Struct<LastFunc<T, TSource, TSourceEnumerator>> NewLastFunc<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            return default(ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.Struct<LastFunc<T, TSource, TSourceEnumerator>>);
+        }
+
+        public static T RunLast<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewLastFunc();
+            return source.Run(func);
+        }
+
+        public static JobHandle ScheduleLast<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<T> output
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewLastFunc();
+            return source.Schedule(func, ref output);
+        }
+
+        public static JobHandle<T> ScheduleLast<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewLastFunc();
+            return source.Schedule(func);
+        }
+
+
         public static T LastOrDefault<T, TSource, TSourceEnumerator>(
             this in ValueSequence<T, TSource, TSourceEnumerator> source,
             in T defaultVal = default
@@ -88,6 +285,68 @@ namespace CareBoo.Blinq
             var result = list[list.Length - 1];
             list.Dispose();
             return result;
+        }
+
+        public struct LastOrDefaultFunc<T, TSource, TSourceEnumerator>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            public T Default;
+            public T Invoke(ValueSequence<T, TSource, TSourceEnumerator> arg0)
+            {
+                return arg0.LastOrDefault(in Default);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.Struct<LastOrDefaultFunc<T, TSource, TSourceEnumerator>> NewLastOrDefaultFunc<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = new LastOrDefaultFunc<T, TSource, TSourceEnumerator> { Default = defaultVal };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.New(func);
+        }
+
+        public static T RunLastOrDefault<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewLastOrDefaultFunc(in defaultVal);
+            return source.Run(func);
+        }
+
+        public static JobHandle ScheduleLastOrDefault<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<T> output,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewLastOrDefaultFunc(in defaultVal);
+            return source.Schedule(func, ref output);
+        }
+
+        public static JobHandle<T> ScheduleLastOrDefault<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewLastOrDefaultFunc(in defaultVal);
+            return source.Schedule(func);
         }
     }
 }
