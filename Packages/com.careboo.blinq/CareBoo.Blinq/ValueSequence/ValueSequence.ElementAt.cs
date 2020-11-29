4e6e6b1c53527dc3ef390451209465e2b52b2736
Jason Bou Kheir <jason.boukheir@pm.me>
2020-11-28 21:39:34 -0800

    feat: :sparkles: Implement RunElementAt and ScheduleElementAt
    Fixes #72

--- Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.ElementAt.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.ElementAt.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using CareBoo.Burst.Delegates;
 using Unity.Collections;
+using Unity.Jobs;
 
 namespace CareBoo.Blinq
 {
@@ -7,7 +9,7 @@ namespace CareBoo.Blinq
     {
         public static T ElementAt<T, TSource, TSourceEnumerator>(
             this in ValueSequence<T, TSource, TSourceEnumerator> source,
-            in int index
+            int index
             )
             where T : struct
             where TSource : struct, ISequence<T, TSourceEnumerator>
@@ -19,9 +21,72 @@ namespace CareBoo.Blinq
             return result;
         }
 
+        public struct ElementAtFunc<T, TSource, TSourceEnumerator>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            public int Index;
+
+            public T Invoke(ValueSequence<T, TSource, TSourceEnumerator> arg0)
+            {
+                return arg0.ElementAt(Index);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.Struct<ElementAtFunc<T, TSource, TSourceEnumerator>> NewElementAtFunc<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            int index
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = new ElementAtFunc<T, TSource, TSourceEnumerator> { Index = index };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.New(func);
+        }
+
+        public static T RunElementAt<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            int index
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewElementAtFunc(index);
+            return source.Run(func);
+        }
+
+        public static JobHandle ScheduleElementAt<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<T> output,
+            int index
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewElementAtFunc(index);
+            return source.Schedule(func, ref output);
+        }
+
+        public static JobHandle<T> ScheduleElementAt<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            int index
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewElementAtFunc(index);
+            return source.Schedule(func);
+        }
+
         public static T ElementAtOrDefault<T, TSource, TSourceEnumerator>(
             this in ValueSequence<T, TSource, TSourceEnumerator> source,
-            in int index,
+            int index,
             in T defaultVal = default
             )
             where T : struct
@@ -34,6 +99,74 @@ namespace CareBoo.Blinq
                 : defaultVal;
             list.Dispose();
             return result;
+        }
+
+        public struct ElementAtOrDefaultFunc<T, TSource, TSourceEnumerator>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            public int Index;
+            public T Default;
+
+            public T Invoke(ValueSequence<T, TSource, TSourceEnumerator> arg0)
+            {
+                return arg0.ElementAt(Index);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.Struct<ElementAtOrDefaultFunc<T, TSource, TSourceEnumerator>> NewElementAtOrDefaultFunc<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            int index,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = new ElementAtOrDefaultFunc<T, TSource, TSourceEnumerator> { Index = index, Default = defaultVal };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.New(func);
+        }
+
+        public static T RunElementAtOrDefault<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            int index,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewElementAtOrDefaultFunc(index, in defaultVal);
+            return source.Run(func);
+        }
+
+        public static JobHandle ScheduleElementAtOrDefault<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<T> output,
+            int index,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewElementAtOrDefaultFunc(index, in defaultVal);
+            return source.Schedule(func, ref output);
+        }
+
+        public static JobHandle<T> ScheduleElementAtOrDefault<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            int index,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.NewElementAtOrDefaultFunc(index, in defaultVal);
+            return source.Schedule(func);
         }
     }
 }
