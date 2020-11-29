cef79693c276aacd1fd070cfc15fa82b6e878525
Jason Bou Kheir <jason.boukheir@pm.me>
2020-11-28 22:32:19 -0800

    test: :white_check_mark: Add First and Last test cases

--- Packages/com.careboo.blinq/CareBoo.Blinq.Tests/LastTest.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq.Tests/LastTest.cs
@@ -30,23 +30,81 @@ internal class LastTest
     [Test, Parallelizable, Timeout(5000)]
     public void BlinqShouldEqualLinqNativeSequenceLast([ArrayValues] int[] sourceArr)
     {
-        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
-        var source = sourceNativeArr.ToValueSequence();
-        var expected = ExceptionAndValue(() => Linq.Last(source));
-        var actual = ExceptionAndValue(() => source.Last());
-        AssertAreEqual(expected, actual);
-        sourceNativeArr.Dispose();
+        using (var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent))
+        {
+            var source = sourceNativeArr.ToValueSequence();
+            var expected = ExceptionAndValue(() => Linq.Last(source));
+            var actual = ExceptionAndValue(() => source.Last());
+            AssertAreEqual(expected, actual);
+        }
     }
 
     [Test, Parallelizable, Timeout(5000)]
     public void BlinqShouldEqualLinqNativeSequenceLastPredicate([ArrayValues] int[] sourceArr)
     {
-        var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent);
-        var source = sourceNativeArr.ToValueSequence();
-        var expected = ExceptionAndValue(() => Linq.Last(source, EqualsZero.Invoke));
-        var actual = ExceptionAndValue(() => source.Last(EqualsZero));
-        AssertAreEqual(expected, actual);
-        sourceNativeArr.Dispose();
+        using (var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent))
+        {
+            var source = sourceNativeArr.ToValueSequence();
+            var expected = ExceptionAndValue(() => Linq.Last(source, EqualsZero.Invoke));
+            var actual = ExceptionAndValue(() => source.Last(EqualsZero));
+            AssertAreEqual(expected, actual);
+        }
+    }
+
+    [Test, Parallelizable, Timeout(5000)]
+    public void BlinqShouldEqualLinqNativeSequenceRunLast([ArrayValues] int[] sourceArr)
+    {
+        using (var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent))
+        {
+            var source = sourceNativeArr.ToValueSequence();
+            var expected = ExceptionAndValue(() => Linq.Last(source));
+            if (expected.exception != null)
+                return;
+            var actual = ExceptionAndValue(() => source.RunLast());
+            AssertAreEqual(expected, actual);
+        }
+    }
+
+    [Test, Parallelizable, Timeout(5000)]
+    public void BlinqShouldEqualLinqNativeSequenceRunLastPredicate([ArrayValues] int[] sourceArr)
+    {
+        using (var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent))
+        {
+            var source = sourceNativeArr.ToValueSequence();
+            var expected = ExceptionAndValue(() => Linq.Last(source, EqualsZero.Invoke));
+            if (expected.exception != null)
+                return;
+            var actual = ExceptionAndValue(() => source.RunLast(EqualsZero));
+            AssertAreEqual(expected, actual);
+        }
+    }
+
+    [Test, Parallelizable, Timeout(5000)]
+    public void BlinqShouldEqualLinqNativeSequenceScheduleLast([ArrayValues] int[] sourceArr)
+    {
+        using (var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent))
+        {
+            var source = sourceNativeArr.ToValueSequence();
+            var expected = ExceptionAndValue(() => Linq.Last(source));
+            if (expected.exception != null)
+                return;
+            var actual = ExceptionAndValue(() => source.ScheduleLast().Complete());
+            AssertAreEqual(expected, actual);
+        }
+    }
+
+    [Test, Parallelizable, Timeout(5000)]
+    public void BlinqShouldEqualLinqNativeSequenceScheduleLastPredicate([ArrayValues] int[] sourceArr)
+    {
+        using (var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent))
+        {
+            var source = sourceNativeArr.ToValueSequence();
+            var expected = ExceptionAndValue(() => Linq.Last(source, EqualsZero.Invoke));
+            if (expected.exception != null)
+                return;
+            var actual = ExceptionAndValue(() => source.ScheduleLast(EqualsZero).Complete());
+            AssertAreEqual(expected, actual);
+        }
     }
 
     [Test, Parallelizable, Timeout(5000)]
@@ -89,5 +147,61 @@ internal class LastTest
         var actual = ExceptionAndValue(() => source.LastOrDefault(EqualsZero));
         AssertAreEqual(expected, actual);
         sourceNativeArr.Dispose();
+    }
+
+    [Test, Parallelizable, Timeout(5000)]
+    public void BlinqShouldEqualLinqNativeSequenceRunLastOrDefault([ArrayValues] int[] sourceArr)
+    {
+        using (var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent))
+        {
+            var source = sourceNativeArr.ToValueSequence();
+            var expected = ExceptionAndValue(() => Linq.Last(source));
+            if (expected.exception != null)
+                return;
+            var actual = ExceptionAndValue(() => source.RunLastOrDefault());
+            AssertAreEqual(expected, actual);
+        }
+    }
+
+    [Test, Parallelizable, Timeout(5000)]
+    public void BlinqShouldEqualLinqNativeSequenceRunLastOrDefaultPredicate([ArrayValues] int[] sourceArr)
+    {
+        using (var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent))
+        {
+            var source = sourceNativeArr.ToValueSequence();
+            var expected = ExceptionAndValue(() => Linq.Last(source, EqualsZero.Invoke));
+            if (expected.exception != null)
+                return;
+            var actual = ExceptionAndValue(() => source.RunLastOrDefault(EqualsZero));
+            AssertAreEqual(expected, actual);
+        }
+    }
+
+    [Test, Parallelizable, Timeout(5000)]
+    public void BlinqShouldEqualLinqNativeSequenceScheduleLastOrDefault([ArrayValues] int[] sourceArr)
+    {
+        using (var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent))
+        {
+            var source = sourceNativeArr.ToValueSequence();
+            var expected = ExceptionAndValue(() => Linq.Last(source));
+            if (expected.exception != null)
+                return;
+            var actual = ExceptionAndValue(() => source.ScheduleLastOrDefault().Complete());
+            AssertAreEqual(expected, actual);
+        }
+    }
+
+    [Test, Parallelizable, Timeout(5000)]
+    public void BlinqShouldEqualLinqNativeSequenceScheduleLastOrDefaultPredicate([ArrayValues] int[] sourceArr)
+    {
+        using (var sourceNativeArr = new NativeArray<int>(sourceArr, Allocator.Persistent))
+        {
+            var source = sourceNativeArr.ToValueSequence();
+            var expected = ExceptionAndValue(() => Linq.Last(source, EqualsZero.Invoke));
+            if (expected.exception != null)
+                return;
+            var actual = ExceptionAndValue(() => source.ScheduleLastOrDefault(EqualsZero).Complete());
+            AssertAreEqual(expected, actual);
+        }
     }
 }
