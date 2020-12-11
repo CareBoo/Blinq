d2aaa73bd91d12b7b6bc8d2fba50b06076f1418b
Jason Bou Kheir <jason.boukheir@pm.me>
2020-12-10 23:47:43 -0800

    feat: :sparkles: Implement RunCount and ScheduleCount
    Fixes #71

--- Packages/com.careboo.blinq/CareBoo.Blinq.PerformanceTests/CountPerformanceTest.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq.PerformanceTests/CountPerformanceTest.cs
@@ -2,54 +2,22 @@ using NUnit.Framework;
 using Unity.PerformanceTesting;
 using Linq = System.Linq.Enumerable;
 using Blinq = CareBoo.Blinq.Sequence;
-using Unity.Jobs;
-using Unity.Burst;
-using Unity.Collections;
 using static ValueFuncs;
-using CareBoo.Burst.Delegates;
-
-[BurstCompile]
-internal struct CountArrayPredicateJob<TPredicate> : IJob
-    where TPredicate : struct, IFunc<int, bool>
-{
-    [ReadOnly]
-    public NativeArray<int> Source;
-
-    public ValueFunc<int, bool>.Struct<TPredicate> Predicate;
-
-    public void Execute()
-    {
-        Blinq.Count(Source, Predicate);
-    }
-}
-
-[BurstCompile]
-internal struct CountSequencePredicateJob<TPredicate> : IJob
-    where TPredicate : struct, IFunc<int, bool>
-{
-    [ReadOnly]
-    public NativeArray<int> Source;
-
-    public ValueFunc<int, bool>.Struct<TPredicate> Predicate;
-
-    public void Execute()
-    {
-        Blinq.Count(Blinq.ToValueSequence(Source), Predicate);
-    }
-}
+using CareBoo.Blinq;
 
 internal class CountPerformanceTest : BaseBlinqPerformanceTest
 {
     [Test, Performance, Category("Performance")]
     public void BlinqArrayPredicate()
     {
-        MakeMeasurement(() => new CountArrayPredicateJob<Functions.EqualsZero> { Source = source, Predicate = EqualsZero }.Run(), "Blinq_Array").Run();
+        MakeMeasurement(() => Blinq.RunCount(in source, EqualsZero), "Blinq_Array").Run();
     }
 
     [Test, Performance, Category("Performance")]
     public void BlinqSequencePredicate()
     {
-        MakeMeasurement(() => new CountSequencePredicateJob<Functions.EqualsZero> { Source = source, Predicate = EqualsZero }.Run(), "Blinq_Sequence").Run();
+        var sourceSequence = source.ToValueSequence();
+        MakeMeasurement(() => Blinq.RunCount(in sourceSequence, EqualsZero), "Blinq_Sequence").Run();
     }
 
     [Test, Performance, Category("Performance")]
--- Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Count.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Count.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Collections;
 using CareBoo.Burst.Delegates;
+using Unity.Jobs;
 
 namespace CareBoo.Blinq
 {
@@ -19,10 +20,116 @@ namespace CareBoo.Blinq
             return count;
         }
 
+        public struct SequenceCountFunc<T, TPredicate>
+            : IFunc<NativeArray<T>, int>
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            public ValueFunc<T, bool>.Struct<TPredicate> Predicate;
+
+            public int Invoke(NativeArray<T> arg0)
+            {
+                return arg0.Count(Predicate);
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, int>.Struct<SequenceCountFunc<T, TPredicate>>
+        NewSequenceCountFunc<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var funcStruct = new SequenceCountFunc<T, TPredicate> { Predicate = predicate };
+            return ValueFunc<NativeArray<T>, int>.New<SequenceCountFunc<T, TPredicate>>(funcStruct);
+        }
+
+        public static int RunCount<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = source.NewSequenceCountFunc(predicate);
+            return source.Run(func);
+        }
+
+        public static JobHandle<int> ScheduleCount<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = source.NewSequenceCountFunc(predicate);
+            return source.Schedule(func);
+        }
+
+        public static JobHandle ScheduleCount<T, TPredicate>(
+            this in NativeArray<T> source,
+            ref NativeArray<int> output,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = source.NewSequenceCountFunc(predicate);
+            return source.Schedule(func, ref output);
+        }
+
         public static int Count<T>(this in NativeArray<T> source)
             where T : struct
         {
             return source.Length;
+        }
+
+        public struct SequenceCountFunc<T>
+            : IFunc<NativeArray<T>, int>
+            where T : struct
+        {
+            public int Invoke(NativeArray<T> arg0)
+            {
+                return arg0.Count();
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, int>.Struct<SequenceCountFunc<T>>
+        NewSequenceCountFunc<T>(
+            this in NativeArray<T> source
+            )
+            where T : struct
+        {
+            return ValueFunc<NativeArray<T>, int>.New<SequenceCountFunc<T>>();
+        }
+
+        public static int RunCount<T>(
+            this in NativeArray<T> source
+            )
+            where T : struct
+        {
+            var func = source.NewSequenceCountFunc();
+            return source.Run(func);
+        }
+
+        public static JobHandle<int> ScheduleCount<T>(
+            this in NativeArray<T> source
+            )
+            where T : struct
+        {
+            var func = source.NewSequenceCountFunc();
+            return source.Schedule(func);
+        }
+
+        public static JobHandle ScheduleCount<T>(
+            this in NativeArray<T> source,
+            ref NativeArray<int> output
+            )
+            where T : struct
+        {
+            var func = source.NewSequenceCountFunc();
+            return source.Schedule(func, ref output);
         }
     }
 }
