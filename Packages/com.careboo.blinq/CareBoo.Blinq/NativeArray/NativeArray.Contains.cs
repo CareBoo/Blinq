5dd471f63fa2da39f63d1be923e71434ae3676cb
Jason Bou Kheir <jason.boukheir@pm.me>
2020-12-13 21:20:13 -0800

    feat: :sparkles: Implement Run and Schedule Contains
    Fixes #70

--- Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Contains.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Contains.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using CareBoo.Burst.Delegates;
 using Unity.Collections;
+using Unity.Jobs;
 
 namespace CareBoo.Blinq
 {
@@ -18,6 +20,60 @@ namespace CareBoo.Blinq
             return false;
         }
 
+        public struct ArrayContainsFunc<T>
+            : IFunc<NativeArray<T>, bool>
+            where T : struct, IEquatable<T>
+        {
+            public T Item;
+
+            public bool Invoke(NativeArray<T> arr)
+            {
+                return arr.Contains(Item);
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, bool>.Struct<ArrayContainsFunc<T>>
+        ContainsAsFunc<T>(
+            this in NativeArray<T> source,
+            in T item
+            )
+            where T : struct, IEquatable<T>
+        {
+            var funcStruct = new ArrayContainsFunc<T> { Item = item };
+            return ValueFunc<NativeArray<T>, bool>.New(funcStruct);
+        }
+
+        public static bool RunContains<T>(
+            this in NativeArray<T> source,
+            in T item
+            )
+            where T : struct, IEquatable<T>
+        {
+            var func = source.ContainsAsFunc(item);
+            return source.Run(func);
+        }
+
+        public static JobHandle<bool> ScheduleContains<T>(
+            this in NativeArray<T> source,
+            in T item
+            )
+            where T : struct, IEquatable<T>
+        {
+            var func = source.ContainsAsFunc(item);
+            return source.Schedule(func);
+        }
+
+        public static JobHandle ScheduleContains<T>(
+            this in NativeArray<T> source,
+            ref NativeArray<bool> output,
+            in T item
+            )
+            where T : struct, IEquatable<T>
+        {
+            var func = source.ContainsAsFunc(item);
+            return source.Schedule(func, ref output);
+        }
+
         public static bool Contains<T, TComparer>(
             this in NativeArray<T> source,
             in T item,
@@ -30,6 +86,70 @@ namespace CareBoo.Blinq
                 if (comparer.Equals(source[i], item))
                     return true;
             return false;
+        }
+
+        public struct ArrayContainsFunc<T, TComparer>
+            : IFunc<NativeArray<T>, bool>
+            where T : struct
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            public T Item;
+            public TComparer Comparer;
+
+            public bool Invoke(NativeArray<T> arr)
+            {
+                return arr.Contains(Item, Comparer);
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, bool>.Struct<ArrayContainsFunc<T, TComparer>>
+        ContainsAsFunc<T, TComparer>(
+            this in NativeArray<T> source,
+            in T item,
+            in TComparer comparer
+            )
+            where T : struct
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            var funcStruct = new ArrayContainsFunc<T, TComparer> { Item = item, Comparer = comparer };
+            return ValueFunc<NativeArray<T>, bool>.New(funcStruct);
+        }
+
+        public static bool RunContains<T, TComparer>(
+            this in NativeArray<T> source,
+            in T item,
+            in TComparer comparer
+            )
+            where T : struct
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            var func = source.ContainsAsFunc(item, comparer);
+            return source.Run(func);
+        }
+
+        public static JobHandle<bool> ScheduleContains<T, TComparer>(
+            this in NativeArray<T> source,
+            in T item,
+            in TComparer comparer
+            )
+            where T : struct
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            var func = source.ContainsAsFunc(item, comparer);
+            return source.Schedule(func);
+        }
+
+        public static JobHandle ScheduleContains<T, TComparer>(
+            this in NativeArray<T> source,
+            ref NativeArray<bool> output,
+            in T item,
+            in TComparer comparer
+            )
+            where T : struct
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            var func = source.ContainsAsFunc(item, comparer);
+            return source.Schedule(func, ref output);
         }
     }
 }
--- Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Contains.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Contains.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using CareBoo.Burst.Delegates;
 using Unity.Collections;
+using Unity.Jobs;
 
 namespace CareBoo.Blinq
 {
@@ -25,6 +27,70 @@ namespace CareBoo.Blinq
             return false;
         }
 
+        public struct SequenceContainsFunc<T, TSource, TSourceEnumerator>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, bool>
+            where T : struct, IEquatable<T>
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            public T Item;
+
+            public bool Invoke(ValueSequence<T, TSource, TSourceEnumerator> arg0)
+            {
+                return arg0.Contains(Item);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, bool>.Struct<SequenceContainsFunc<T, TSource, TSourceEnumerator>>
+        ContainsAsFunc<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            in T item
+            )
+            where T : struct, IEquatable<T>
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var funcStruct = new SequenceContainsFunc<T, TSource, TSourceEnumerator> { Item = item };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, bool>.New(funcStruct);
+        }
+
+        public static bool RunContains<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            in T item
+            )
+            where T : struct, IEquatable<T>
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.ContainsAsFunc(item);
+            return source.Run(func);
+        }
+
+        public static JobHandle<bool> ScheduleContains<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            in T item
+            )
+            where T : struct, IEquatable<T>
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.ContainsAsFunc(item);
+            return source.Schedule(func);
+        }
+
+        public static JobHandle ScheduleContains<T, TSource, TSourceEnumerator>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<bool> output,
+            in T item
+            )
+            where T : struct, IEquatable<T>
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+        {
+            var func = source.ContainsAsFunc(item);
+            return source.Schedule(func, ref output);
+        }
+
         public static bool Contains<T, TSource, TSourceEnumerator, TComparer>(
             this in ValueSequence<T, TSource, TSourceEnumerator> source,
             in T item,
@@ -44,6 +110,80 @@ namespace CareBoo.Blinq
                 }
             sourceList.Dispose();
             return false;
+        }
+
+        public struct SequenceContainsFunc<T, TSource, TSourceEnumerator, TComparer>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, bool>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            public T Item;
+            public TComparer Comparer;
+
+            public bool Invoke(ValueSequence<T, TSource, TSourceEnumerator> seq)
+            {
+                return seq.Contains(Item, Comparer);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, bool>.Struct<SequenceContainsFunc<T, TSource, TSourceEnumerator, TComparer>>
+        ContainsAsFunc<T, TSource, TSourceEnumerator, TComparer>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            in T item,
+            in TComparer comparer
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            var funcStruct = new SequenceContainsFunc<T, TSource, TSourceEnumerator, TComparer> { Item = item, Comparer = comparer };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, bool>.New(funcStruct);
+        }
+
+        public static bool RunContains<T, TSource, TSourceEnumerator, TComparer>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            in T item,
+            in TComparer comparer
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            var func = source.ContainsAsFunc(item, comparer);
+            return source.Run(func);
+        }
+
+        public static JobHandle<bool> ScheduleContains<T, TSource, TSourceEnumerator, TComparer>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            in T item,
+            in TComparer comparer
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            var func = source.ContainsAsFunc(item, comparer);
+            return source.Schedule(func);
+        }
+
+        public static JobHandle ScheduleContains<T, TSource, TSourceEnumerator, TComparer>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<bool> output,
+            in T item,
+            in TComparer comparer
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TComparer : struct, IEqualityComparer<T>
+        {
+            var func = source.ContainsAsFunc(item, comparer);
+            return source.Schedule(func, ref output);
         }
     }
 }
