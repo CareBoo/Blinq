9a73d5312910fb8bff9db002eb8ab57ea38b4183
Jason Bou Kheir <jason.boukheir@pm.me>
2020-12-18 00:39:48 -0800

    feat: :sparkles: Implement RunAll and ScheduleAll
    Fixes #68

--- Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.All.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.All.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using CareBoo.Burst.Delegates;
 using Unity.Collections;
+using Unity.Jobs;
 
 namespace CareBoo.Blinq
 {
@@ -26,5 +27,73 @@ namespace CareBoo.Blinq
             return true;
         }
 
+        public struct SequenceAllFunc<T, TSource, TSourceEnumerator, TPredicate>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, bool>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            public ValueFunc<T, bool>.Struct<TPredicate> Predicate;
+
+            public bool Invoke(ValueSequence<T, TSource, TSourceEnumerator> seq)
+            {
+                return seq.All(Predicate);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, bool>.Struct<SequenceAllFunc<T, TSource, TSourceEnumerator, TPredicate>>
+        AllAsFunc<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var allFunc = new SequenceAllFunc<T, TSource, TSourceEnumerator, TPredicate> { Predicate = predicate };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, bool>.New(allFunc);
+        }
+
+        public static bool RunAll<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var allFunc = source.AllAsFunc(predicate);
+            return source.Run(allFunc);
+        }
+
+        public static JobHandle<bool> ScheduleAll<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var allFunc = source.AllAsFunc(predicate);
+            return source.Schedule(allFunc);
+        }
+
+        public static JobHandle ScheduleAll<T, TSource, TSourceEnumerator, TPredicate>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<bool> output,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var allFunc = source.AllAsFunc(predicate);
+            return source.Schedule(allFunc, ref output);
+        }
     }
 }
