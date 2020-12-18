751007ada130657a52f3136c5a1f2de05aa54eed
Jason Bou Kheir <jason.boukheir@pm.me>
2020-12-18 00:39:48 -0800

    feat: :sparkles: Implement RunAll and ScheduleAll
    Fixes #68

--- Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.All.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.All.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Collections;
 using CareBoo.Burst.Delegates;
+using Unity.Jobs;
 
 namespace CareBoo.Blinq
 {
@@ -16,6 +17,65 @@ namespace CareBoo.Blinq
                 if (!predicate.Invoke(source[i]))
                     return false;
             return true;
+        }
+
+        public struct ArrayAllFunc<T, TPredicate>
+            : IFunc<NativeArray<T>, bool>
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            public ValueFunc<T, bool>.Struct<TPredicate> Predicate;
+
+            public bool Invoke(NativeArray<T> seq)
+            {
+                return seq.All(Predicate);
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, bool>.Struct<ArrayAllFunc<T, TPredicate>>
+        AllAsFunc<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var allFunc = new ArrayAllFunc<T, TPredicate> { Predicate = predicate };
+            return ValueFunc<NativeArray<T>, bool>.New(allFunc);
+        }
+
+        public static bool RunAll<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var allFunc = source.AllAsFunc(predicate);
+            return source.Run(allFunc);
+        }
+
+        public static JobHandle<bool> ScheduleAll<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var allFunc = source.AllAsFunc(predicate);
+            return source.Schedule(allFunc);
+        }
+
+        public static JobHandle ScheduleAll<T, TPredicate>(
+            this in NativeArray<T> source,
+            ref NativeArray<bool> output,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var allFunc = source.AllAsFunc(predicate);
+            return source.Schedule(allFunc, ref output);
         }
     }
 }
