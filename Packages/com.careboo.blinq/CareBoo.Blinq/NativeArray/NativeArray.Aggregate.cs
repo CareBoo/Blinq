c089f7e1f52f6e373d4973aa438721c6411e2825
Jason Bou Kheir <jason.boukheir@pm.me>
2020-12-18 00:34:02 -0800

    feat: :sparkles: Implement RunAggregate and ScheduleAggregate
    Fixes #67

--- Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Aggregate.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Aggregate.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Collections;
 using CareBoo.Burst.Delegates;
+using Unity.Jobs;
 
 namespace CareBoo.Blinq
 {
@@ -22,6 +23,90 @@ namespace CareBoo.Blinq
             return resultSelector.Invoke(seed);
         }
 
+        public struct ArrayAggregateFunc<T, TAccumulate, TResult, TFunc, TResultSelector>
+            : IFunc<NativeArray<T>, TResult>
+            where T : struct
+            where TAccumulate : struct
+            where TResult : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+            where TResultSelector : struct, IFunc<TAccumulate, TResult>
+        {
+            public TAccumulate Seed;
+            public ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> Func;
+            public ValueFunc<TAccumulate, TResult>.Struct<TResultSelector> ResultSelector;
+
+            public TResult Invoke(NativeArray<T> seq)
+            {
+                return seq.Aggregate(Seed, Func, ResultSelector);
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, TResult>.Struct<ArrayAggregateFunc<T, TAccumulate, TResult, TFunc, TResultSelector>>
+        AggregateAsFunc<T, TAccumulate, TResult, TFunc, TResultSelector>(
+            this in NativeArray<T> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func,
+            ValueFunc<TAccumulate, TResult>.Struct<TResultSelector> resultSelector
+            )
+            where T : struct
+            where TAccumulate : struct
+            where TResult : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+            where TResultSelector : struct, IFunc<TAccumulate, TResult>
+        {
+            var seqAggregateFunc = new ArrayAggregateFunc<T, TAccumulate, TResult, TFunc, TResultSelector> { Seed = seed, Func = func, ResultSelector = resultSelector };
+            return ValueFunc<NativeArray<T>, TResult>.New(seqAggregateFunc);
+        }
+
+        public static TResult RunAggregate<T, TAccumulate, TResult, TFunc, TResultSelector>(
+            this in NativeArray<T> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func,
+            ValueFunc<TAccumulate, TResult>.Struct<TResultSelector> resultSelector
+            )
+            where T : struct
+            where TAccumulate : struct
+            where TResult : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+            where TResultSelector : struct, IFunc<TAccumulate, TResult>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func, resultSelector);
+            return source.Run(aggregateFunc);
+        }
+
+        public static JobHandle<TResult> ScheduleAggregate<T, TAccumulate, TResult, TFunc, TResultSelector>(
+            this in NativeArray<T> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func,
+            ValueFunc<TAccumulate, TResult>.Struct<TResultSelector> resultSelector
+            )
+            where T : struct
+            where TAccumulate : struct
+            where TResult : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+            where TResultSelector : struct, IFunc<TAccumulate, TResult>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func, resultSelector);
+            return source.Schedule(aggregateFunc);
+        }
+
+        public static JobHandle ScheduleAggregate<T, TAccumulate, TResult, TFunc, TResultSelector>(
+            this in NativeArray<T> source,
+            ref NativeArray<TResult> output,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func,
+            ValueFunc<TAccumulate, TResult>.Struct<TResultSelector> resultSelector
+            )
+            where T : struct
+            where TAccumulate : struct
+            where TResult : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+            where TResultSelector : struct, IFunc<TAccumulate, TResult>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func, resultSelector);
+            return source.Schedule(aggregateFunc, ref output);
+        }
+
         public static TAccumulate Aggregate<T, TAccumulate, TFunc>(
             this in NativeArray<T> source,
             TAccumulate seed,
@@ -36,6 +121,75 @@ namespace CareBoo.Blinq
             return seed;
         }
 
+        public struct ArrayAggregateFunc<T, TAccumulate, TFunc>
+            : IFunc<NativeArray<T>, TAccumulate>
+            where T : struct
+            where TAccumulate : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+        {
+            public TAccumulate Seed;
+            public ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> Func;
+
+            public TAccumulate Invoke(NativeArray<T> seq)
+            {
+                return seq.Aggregate(Seed, Func);
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, TAccumulate>.Struct<ArrayAggregateFunc<T, TAccumulate, TFunc>>
+        AggregateAsFunc<T, TAccumulate, TFunc>(
+            this in NativeArray<T> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func
+            )
+            where T : struct
+            where TAccumulate : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+        {
+            var seqAggregateFunc = new ArrayAggregateFunc<T, TAccumulate, TFunc> { Seed = seed, Func = func };
+            return ValueFunc<NativeArray<T>, TAccumulate>.New(seqAggregateFunc);
+        }
+
+        public static TAccumulate RunAggregate<T, TAccumulate, TFunc>(
+            this in NativeArray<T> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func
+            )
+            where T : struct
+            where TAccumulate : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func);
+            return source.Run(aggregateFunc);
+        }
+
+        public static JobHandle<TAccumulate> ScheduleAggregate<T, TAccumulate, TFunc>(
+            this in NativeArray<T> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func
+            )
+            where T : struct
+            where TAccumulate : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func);
+            return source.Schedule(aggregateFunc);
+        }
+
+        public static JobHandle ScheduleAggregate<T, TAccumulate, TFunc>(
+            this in NativeArray<T> source,
+            ref NativeArray<TAccumulate> output,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func
+            )
+            where T : struct
+            where TAccumulate : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func);
+            return source.Schedule(aggregateFunc, ref output);
+        }
+
         public static T Aggregate<T, TFunc>(
             this in NativeArray<T> source,
             ValueFunc<T, T, T>.Struct<TFunc> func
@@ -48,6 +202,65 @@ namespace CareBoo.Blinq
             for (var i = 1; i < source.Length; i++)
                 seed = func.Invoke(seed, source[i]);
             return seed;
+        }
+
+        public struct ArrayAggregateFunc<T, TFunc>
+            : IFunc<NativeArray<T>, T>
+            where T : struct
+            where TFunc : struct, IFunc<T, T, T>
+        {
+            public ValueFunc<T, T, T>.Struct<TFunc> Func;
+
+            public T Invoke(NativeArray<T> seq)
+            {
+                return seq.Aggregate(Func);
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, T>.Struct<ArrayAggregateFunc<T, TFunc>>
+        AggregateAsFunc<T, TFunc>(
+            this in NativeArray<T> source,
+            ValueFunc<T, T, T>.Struct<TFunc> func
+            )
+            where T : struct
+            where TFunc : struct, IFunc<T, T, T>
+        {
+            var seqAggregateFunc = new ArrayAggregateFunc<T, TFunc> { Func = func };
+            return ValueFunc<NativeArray<T>, T>.New(seqAggregateFunc);
+        }
+
+        public static T RunAggregate<T, TFunc>(
+            this in NativeArray<T> source,
+            ValueFunc<T, T, T>.Struct<TFunc> func
+            )
+            where T : struct
+            where TFunc : struct, IFunc<T, T, T>
+        {
+            var aggregateFunc = source.AggregateAsFunc(func);
+            return source.Run(aggregateFunc);
+        }
+
+        public static JobHandle<T> ScheduleAggregate<T, TFunc>(
+            this in NativeArray<T> source,
+            ValueFunc<T, T, T>.Struct<TFunc> func
+            )
+            where T : struct
+            where TFunc : struct, IFunc<T, T, T>
+        {
+            var aggregateFunc = source.AggregateAsFunc(func);
+            return source.Schedule(aggregateFunc);
+        }
+
+        public static JobHandle ScheduleAggregate<T, TFunc>(
+            this in NativeArray<T> source,
+            ref NativeArray<T> output,
+            ValueFunc<T, T, T>.Struct<TFunc> func
+            )
+            where T : struct
+            where TFunc : struct, IFunc<T, T, T>
+        {
+            var aggregateFunc = source.AggregateAsFunc(func);
+            return source.Schedule(aggregateFunc, ref output);
         }
     }
 }
--- Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Aggregate.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/ValueSequence/ValueSequence.Aggregate.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using CareBoo.Burst.Delegates;
 using Unity.Collections;
+using Unity.Jobs;
 
 namespace CareBoo.Blinq
 {
@@ -28,6 +29,100 @@ namespace CareBoo.Blinq
             return result;
         }
 
+        public struct SequenceAggregateFunc<T, TSource, TSourceEnumerator, TAccumulate, TResult, TFunc, TResultSelector>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, TResult>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TAccumulate : struct
+            where TResult : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+            where TResultSelector : struct, IFunc<TAccumulate, TResult>
+        {
+            public TAccumulate Seed;
+            public ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> Func;
+            public ValueFunc<TAccumulate, TResult>.Struct<TResultSelector> ResultSelector;
+
+            public TResult Invoke(ValueSequence<T, TSource, TSourceEnumerator> seq)
+            {
+                return seq.Aggregate(Seed, Func, ResultSelector);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, TResult>.Struct<SequenceAggregateFunc<T, TSource, TSourceEnumerator, TAccumulate, TResult, TFunc, TResultSelector>>
+        AggregateAsFunc<T, TSource, TSourceEnumerator, TAccumulate, TResult, TFunc, TResultSelector>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func,
+            ValueFunc<TAccumulate, TResult>.Struct<TResultSelector> resultSelector
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TAccumulate : struct
+            where TResult : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+            where TResultSelector : struct, IFunc<TAccumulate, TResult>
+        {
+            var seqAggregateFunc = new SequenceAggregateFunc<T, TSource, TSourceEnumerator, TAccumulate, TResult, TFunc, TResultSelector> { Seed = seed, Func = func, ResultSelector = resultSelector };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, TResult>.New(seqAggregateFunc);
+        }
+
+        public static TResult RunAggregate<T, TSource, TSourceEnumerator, TAccumulate, TResult, TFunc, TResultSelector>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func,
+            ValueFunc<TAccumulate, TResult>.Struct<TResultSelector> resultSelector
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TAccumulate : struct
+            where TResult : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+            where TResultSelector : struct, IFunc<TAccumulate, TResult>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func, resultSelector);
+            return source.Run(aggregateFunc);
+        }
+
+        public static JobHandle<TResult> ScheduleAggregate<T, TSource, TSourceEnumerator, TAccumulate, TResult, TFunc, TResultSelector>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func,
+            ValueFunc<TAccumulate, TResult>.Struct<TResultSelector> resultSelector
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TAccumulate : struct
+            where TResult : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+            where TResultSelector : struct, IFunc<TAccumulate, TResult>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func, resultSelector);
+            return source.Schedule(aggregateFunc);
+        }
+
+        public static JobHandle ScheduleAggregate<T, TSource, TSourceEnumerator, TAccumulate, TResult, TFunc, TResultSelector>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<TResult> output,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func,
+            ValueFunc<TAccumulate, TResult>.Struct<TResultSelector> resultSelector
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TAccumulate : struct
+            where TResult : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+            where TResultSelector : struct, IFunc<TAccumulate, TResult>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func, resultSelector);
+            return source.Schedule(aggregateFunc, ref output);
+        }
+
         public static TAccumulate Aggregate<T, TSource, TSourceEnumerator, TAccumulate, TFunc>(
             this in ValueSequence<T, TSource, TSourceEnumerator> source,
             TAccumulate seed,
@@ -44,6 +139,85 @@ namespace CareBoo.Blinq
                 seed = func.Invoke(seed, sourceList[i]);
             sourceList.Dispose();
             return seed;
+        }
+
+        public struct SequenceAggregateFunc<T, TSource, TSourceEnumerator, TAccumulate, TFunc>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, TAccumulate>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TAccumulate : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+        {
+            public TAccumulate Seed;
+            public ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> Func;
+
+            public TAccumulate Invoke(ValueSequence<T, TSource, TSourceEnumerator> seq)
+            {
+                return seq.Aggregate(Seed, Func);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, TAccumulate>.Struct<SequenceAggregateFunc<T, TSource, TSourceEnumerator, TAccumulate, TFunc>>
+        AggregateAsFunc<T, TSource, TSourceEnumerator, TAccumulate, TFunc>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TAccumulate : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+        {
+            var seqAggregateFunc = new SequenceAggregateFunc<T, TSource, TSourceEnumerator, TAccumulate, TFunc> { Seed = seed, Func = func };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, TAccumulate>.New(seqAggregateFunc);
+        }
+
+        public static TAccumulate RunAggregate<T, TSource, TSourceEnumerator, TAccumulate, TFunc>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TAccumulate : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func);
+            return source.Run(aggregateFunc);
+        }
+
+        public static JobHandle<TAccumulate> ScheduleAggregate<T, TSource, TSourceEnumerator, TAccumulate, TFunc>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TAccumulate : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func);
+            return source.Schedule(aggregateFunc);
+        }
+
+        public static JobHandle ScheduleAggregate<T, TSource, TSourceEnumerator, TAccumulate, TFunc>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<TAccumulate> output,
+            TAccumulate seed,
+            ValueFunc<TAccumulate, T, TAccumulate>.Struct<TFunc> func
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TAccumulate : struct
+            where TFunc : struct, IFunc<TAccumulate, T, TAccumulate>
+        {
+            var aggregateFunc = source.AggregateAsFunc(seed, func);
+            return source.Schedule(aggregateFunc, ref output);
         }
 
         public static T Aggregate<T, TSource, TSourceEnumerator, TFunc>(
@@ -66,6 +240,75 @@ namespace CareBoo.Blinq
                 seed = func.Invoke(seed, sourceList[i]);
             sourceList.Dispose();
             return seed;
+        }
+
+        public struct SequenceAggregateFunc<T, TSource, TSourceEnumerator, TFunc>
+            : IFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TFunc : struct, IFunc<T, T, T>
+        {
+            public ValueFunc<T, T, T>.Struct<TFunc> Func;
+
+            public T Invoke(ValueSequence<T, TSource, TSourceEnumerator> seq)
+            {
+                return seq.Aggregate(Func);
+            }
+        }
+
+        public static ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.Struct<SequenceAggregateFunc<T, TSource, TSourceEnumerator, TFunc>>
+        AggregateAsFunc<T, TSource, TSourceEnumerator, TFunc>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, T, T>.Struct<TFunc> func
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TFunc : struct, IFunc<T, T, T>
+        {
+            var seqAggregateFunc = new SequenceAggregateFunc<T, TSource, TSourceEnumerator, TFunc> { Func = func };
+            return ValueFunc<ValueSequence<T, TSource, TSourceEnumerator>, T>.New(seqAggregateFunc);
+        }
+
+        public static T RunAggregate<T, TSource, TSourceEnumerator, TFunc>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, T, T>.Struct<TFunc> func
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TFunc : struct, IFunc<T, T, T>
+        {
+            var aggregateFunc = source.AggregateAsFunc(func);
+            return source.Run(aggregateFunc);
+        }
+
+        public static JobHandle<T> ScheduleAggregate<T, TSource, TSourceEnumerator, TFunc>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ValueFunc<T, T, T>.Struct<TFunc> func
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TFunc : struct, IFunc<T, T, T>
+        {
+            var aggregateFunc = source.AggregateAsFunc(func);
+            return source.Schedule(aggregateFunc);
+        }
+
+        public static JobHandle ScheduleAggregate<T, TSource, TSourceEnumerator, TFunc>(
+            this in ValueSequence<T, TSource, TSourceEnumerator> source,
+            ref NativeArray<T> output,
+            ValueFunc<T, T, T>.Struct<TFunc> func
+            )
+            where T : struct
+            where TSource : struct, ISequence<T, TSourceEnumerator>
+            where TSourceEnumerator : struct, IEnumerator<T>
+            where TFunc : struct, IFunc<T, T, T>
+        {
+            var aggregateFunc = source.AggregateAsFunc(func);
+            return source.Schedule(aggregateFunc, ref output);
         }
     }
 }
