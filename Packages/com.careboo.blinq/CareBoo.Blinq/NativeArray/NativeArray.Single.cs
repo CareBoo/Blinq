93a4f6ba00df03ac1b7adc09c5bcd0faed230a9e
Jason Bou Kheir <jason.boukheir@pm.me>
2020-12-20 22:34:43 -0800

    feat: :sparkles: Add Run/Schedule NativeArray Single

--- Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Single.cs
+++ Packages/com.careboo.blinq/CareBoo.Blinq/NativeArray/NativeArray.Single.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Unity.Collections;
+﻿using Unity.Collections;
 using CareBoo.Burst.Delegates;
+using Unity.Jobs;
 
 namespace CareBoo.Blinq
 {
@@ -10,21 +10,82 @@ namespace CareBoo.Blinq
             this in NativeArray<T> source,
             ValueFunc<T, bool>.Struct<TPredicate> predicate
             )
-            where T : unmanaged, IEquatable<T>
+            where T : struct
             where TPredicate : struct, IFunc<T, bool>
         {
-            var set = new NativeHashSet<T>(source.Length, Allocator.Temp);
+            T result = default;
+            var isFound = false;
             for (var i = 0; i < source.Length; i++)
             {
                 var val = source[i];
                 if (predicate.Invoke(val))
-                    if (!set.Add(val))
+                {
+                    if (isFound)
                         throw Error.MoreThanOneMatch();
+                    isFound = true;
+                    result = val;
+                }
             }
-            var iter = set.GetEnumerator();
-            if (iter.MoveNext())
-                return iter.Current;
+            if (isFound)
+                return result;
             throw Error.NoMatch();
+        }
+
+        public struct ArraySingleFunc<T, TPredicate>
+            : IFunc<NativeArray<T>, T>
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            public ValueFunc<T, bool>.Struct<TPredicate> Predicate;
+            public T Invoke(NativeArray<T> arg0)
+            {
+                return arg0.Single(Predicate);
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, T>.Struct<ArraySingleFunc<T, TPredicate>> SingleAsFunc<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = new ArraySingleFunc<T, TPredicate> { Predicate = predicate };
+            return ValueFunc<NativeArray<T>, T>.New(func);
+        }
+
+        public static T RunSingle<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var ArraySingleFunc = source.SingleAsFunc(predicate);
+            return source.Run(source.SingleAsFunc(predicate));
+        }
+
+        public static JobHandle ScheduleSingle<T, TPredicate>(
+            this in NativeArray<T> source,
+            ref NativeArray<T> output,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var ArraySingleFunc = source.SingleAsFunc(predicate);
+            return source.Schedule(source.SingleAsFunc(predicate), ref output);
+        }
+
+        public static JobHandle<T> ScheduleSingle<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var ArraySingleFunc = source.SingleAsFunc(predicate);
+            return source.Schedule(source.SingleAsFunc(predicate));
         }
 
         public static T SingleOrDefault<T, TPredicate>(
@@ -32,33 +93,143 @@ namespace CareBoo.Blinq
             ValueFunc<T, bool>.Struct<TPredicate> predicate,
             in T defaultVal = default
             )
-            where T : unmanaged, IEquatable<T>
+            where T : struct
             where TPredicate : struct, IFunc<T, bool>
         {
-            var set = new NativeHashSet<T>(source.Length, Allocator.Temp);
+            T result = default;
+            bool isFound = false;
             for (var i = 0; i < source.Length; i++)
             {
                 var val = source[i];
                 if (predicate.Invoke(val))
-                    if (!set.Add(val))
+                {
+                    if (isFound)
                         throw Error.MoreThanOneMatch();
+                    isFound = true;
+                    result = val;
+                }
             }
-            var iter = set.GetEnumerator();
-            if (iter.MoveNext())
-                return iter.Current;
-            return defaultVal;
+            return isFound ? result : defaultVal;
+        }
+
+        public struct ArraySingleOrDefaultFunc<T, TPredicate>
+            : IFunc<NativeArray<T>, T>
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            public ValueFunc<T, bool>.Struct<TPredicate> Predicate;
+            public T Default;
+            public T Invoke(NativeArray<T> arg0)
+            {
+                return arg0.SingleOrDefault(Predicate, in Default);
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, T>.Struct<ArraySingleOrDefaultFunc<T, TPredicate>> SingleOrDefaultAsFunc<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = new ArraySingleOrDefaultFunc<T, TPredicate> { Predicate = predicate, Default = defaultVal };
+            return ValueFunc<NativeArray<T>, T>.New(func);
+        }
+
+        public static T RunSingleOrDefault<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = source.SingleOrDefaultAsFunc(predicate, in defaultVal);
+            return source.Run(func);
+        }
+
+        public static JobHandle ScheduleSingleOrDefault<T, TPredicate>(
+            this in NativeArray<T> source,
+            ref NativeArray<T> output,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = source.SingleOrDefaultAsFunc(predicate, in defaultVal);
+            return source.Schedule(func, ref output);
+        }
+
+        public static JobHandle<T> ScheduleSingleOrDefault<T, TPredicate>(
+            this in NativeArray<T> source,
+            ValueFunc<T, bool>.Struct<TPredicate> predicate,
+            in T defaultVal = default
+            )
+            where T : struct
+            where TPredicate : struct, IFunc<T, bool>
+        {
+            var func = source.SingleOrDefaultAsFunc(predicate, in defaultVal);
+            return source.Schedule(func);
         }
 
         public static T Single<T>(
             this in NativeArray<T> source
             )
-            where T : unmanaged, IEquatable<T>
+            where T : struct
         {
             if (source.Length > 1)
                 throw Error.MoreThanOneElement();
             if (source.Length == 0)
                 throw Error.NoElements();
             return source[0];
+        }
+
+        public struct ArraySingleFunc<T>
+            : IFunc<NativeArray<T>, T>
+            where T : struct
+        {
+            public T Invoke(NativeArray<T> arg0)
+            {
+                return arg0.Single();
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, T>.Struct<ArraySingleFunc<T>> SingleAsFunc<T>(
+            this in NativeArray<T> source
+            )
+            where T : struct
+        {
+            return default(ValueFunc<NativeArray<T>, T>.Struct<ArraySingleFunc<T>>);
+        }
+
+        public static T RunSingle<T>(
+            this in NativeArray<T> source
+            )
+            where T : struct
+        {
+            var func = source.SingleAsFunc();
+            return source.Run(func);
+        }
+
+        public static JobHandle ScheduleSingle<T>(
+            this in NativeArray<T> source,
+            ref NativeArray<T> output
+            )
+            where T : struct
+        {
+            var func = source.SingleAsFunc();
+            return source.Schedule(func, ref output);
+        }
+
+        public static JobHandle<T> ScheduleSingle<T>(
+            this in NativeArray<T> source
+            )
+            where T : struct
+        {
+            var func = source.SingleAsFunc();
+            return source.Schedule(func);
         }
 
         public static T SingleOrDefault<T>(
@@ -72,6 +243,58 @@ namespace CareBoo.Blinq
             if (source.Length == 0)
                 return defaultVal;
             return source[0];
+        }
+
+        public struct ArraySingleOrDefaultFunc<T>
+            : IFunc<NativeArray<T>, T>
+            where T : struct
+        {
+            public T Default;
+            public T Invoke(NativeArray<T> arg0)
+            {
+                return arg0.SingleOrDefault(in Default);
+            }
+        }
+
+        public static ValueFunc<NativeArray<T>, T>.Struct<ArraySingleOrDefaultFunc<T>> SingleOrDefaultAsFunc<T>(
+            this in NativeArray<T> source,
+            in T defaultVal = default
+            )
+            where T : struct
+        {
+            var func = new ArraySingleOrDefaultFunc<T> { Default = defaultVal };
+            return ValueFunc<NativeArray<T>, T>.New(func);
+        }
+
+        public static T RunSingleOrDefault<T>(
+            this in NativeArray<T> source,
+            in T defaultVal = default
+            )
+            where T : struct
+        {
+            var func = source.SingleOrDefaultAsFunc(in defaultVal);
+            return source.Run(func);
+        }
+
+        public static JobHandle ScheduleSingleOrDefault<T>(
+            this in NativeArray<T> source,
+            ref NativeArray<T> output,
+            in T defaultVal = default
+            )
+            where T : struct
+        {
+            var func = source.SingleOrDefaultAsFunc(in defaultVal);
+            return source.Schedule(func, ref output);
+        }
+
+        public static JobHandle<T> ScheduleSingleOrDefault<T>(
+            this in NativeArray<T> source,
+            in T defaultVal = default
+            )
+            where T : struct
+        {
+            var func = source.SingleOrDefaultAsFunc(in defaultVal);
+            return source.Schedule(func);
         }
     }
 }
